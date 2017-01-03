9cdbdae163bc305b78283548183bc1a0c117cb49
Lawrence Wang <l@lw.io>
2017-01-02 16:52:19 -0800

    make tests pass

---
+++ config/jest/mockFetch.js
@@ -0,0 +1,21 @@
+// source:
+// https://blog.callstack.io/unit-testing-react-native-with-the-new-jest-ii-redux-snapshots-for-your-actions-and-reducers-8559f6f8050b
+
+// Mocking the global.fetch included in React Native
+global.fetch = jest.fn() // eslint-disable-line no-undef
+
+// Helper to mock a success response (only once)
+fetch.mockResponseSuccess = body => {
+  fetch.mockImplementationOnce(
+    () => Promise.resolve({
+      json: () => Promise.resolve(body)
+    })
+  )
+}
+
+// Helper to mock a failure response (only once)
+fetch.mockResponseFailure = error => {
+  fetch.mockImplementationOnce(
+    () => Promise.reject(error)
+  )
+}
