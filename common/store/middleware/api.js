a26b653eb0daa3e73290c43be1bf0c3937c5b3d0
Lawrence Wang <l@lw.io>
2017-03-27 20:52:30 -0700

    DRY up API access

--- common/store/middleware/api.js
+++ common/store/middleware/api.js
@@ -1,9 +1,5 @@
-import { Platform } from 'react-native'
-import { API_HOST, IOS_EMULATOR_API_HOST, ANDROID_EMULATOR_API_HOST } from 'react-native-dotenv'
-import { getSessionCookie, setSessionCookie } from '../../util/session'
-import DeviceInfo from 'react-native-device-info'
-
-const HOST = DeviceInfo.isEmulator() ? (Platform.OS === 'ios' ? IOS_EMULATOR_API_HOST : ANDROID_EMULATOR_API_HOST) : API_HOST
+import { getSessionCookie } from '../../util/session'
+import fetchJSON from '../../util/fetchJSON'
 
 export default function apiMiddleware (store) {
   return next => action => {
@@ -17,30 +13,4 @@ export default function apiMiddleware (store) {
 
     return next({...action, payload: promise})
   }
-}
-
-function fetchJSON (path, params, options = {}) {
-  const { host, method, cookie } = options
-  const url = (host || HOST) + path
-  return fetch(url, {
-    method: method || 'get',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Cookie': cookie
-    },
-    body: JSON.stringify(params)
-  })
-  .then(resp => {
-    let { status, statusText, url } = resp
-    if (status === 200) {
-      return setSessionCookie(resp).then(() => resp.json())
-    }
-
-    return resp.text().then(body => {
-      let error = new Error(body)
-      error.response = {status, statusText, url, body}
-      throw error
-    })
-  })
 }
--- common/util/fetchGraphQL.js
+++ common/util/fetchGraphQL.js
@@ -1,22 +1,8 @@
-import { Platform } from 'react-native'
-import { API_HOST, IOS_EMULATOR_API_HOST, ANDROID_EMULATOR_API_HOST } from 'react-native-dotenv'
 import { getSessionCookie } from './session'
-import DeviceInfo from 'react-native-device-info'
-
-const HOST = DeviceInfo.isEmulator() ? (Platform.OS === 'ios' ? IOS_EMULATOR_API_HOST : ANDROID_EMULATOR_API_HOST) : API_HOST
+import fetchJSON from './fetchJSON'
 
 export default function fetchGraphQL (query) {
   return getSessionCookie()
-  .then(cookie =>
-    fetch(`${HOST}/noo/graphql`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Cookie': cookie
-      },
-      body: JSON.stringify({query})
-    }))
-  .then(resp => resp.json())
+  .then(cookie => fetchJSON('/noo/graphql', {query}, {cookie, method: 'POST'}))
   .then(json => json.data)
 }
