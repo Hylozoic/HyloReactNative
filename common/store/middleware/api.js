ae6d3339148808af81937af973256d8ac4d8b464
Connor Turland <connorturland@gmail.com>
2017-03-29 12:54:22 -0400

    unneccessary file and rename env vars back to normal

--- common/store/middleware/api.js
+++ common/store/middleware/api.js
@@ -1,9 +1,11 @@
 import { Platform } from 'react-native'
-import { API_HOST, IOS_EMULATOR_API_HOST, EMULATOR_API_HOST } from 'react-native-dotenv'
+import { API_HOST, IOS_EMULATOR_API_HOST, ANDROID_EMULATOR_API_HOST } from 'react-native-dotenv'
 import { getSessionCookie, setSessionCookie } from '../../util/session'
 import DeviceInfo from 'react-native-device-info'
 
-const HOST = DeviceInfo.isEmulator() ? (Platform.OS === 'ios' ? IOS_EMULATOR_API_HOST : EMULATOR_API_HOST) : API_HOST
+const HOST = DeviceInfo.isEmulator()
+  ? (Platform.OS === 'ios' ? IOS_EMULATOR_API_HOST : ANDROID_EMULATOR_API_HOST)
+  : API_HOST
 
 export default function apiMiddleware (store) {
   return next => action => {
