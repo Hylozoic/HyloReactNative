f1f26d018c532519cdc2706648c9e8ab9002507e
Lawrence Wang <l@lw.io>
2017-03-30 16:37:53 -0700

    fix URL scheme for google signin

--- common/components/Login/GoogleLoginButton.js
+++ common/components/Login/GoogleLoginButton.js
@@ -1,13 +1,13 @@
 import React, { PropTypes } from 'react'
 import { IOS_GOOGLE_CLIENT_ID, WEB_GOOGLE_CLIENT_ID } from 'react-native-dotenv'
-import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin'
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin'
 
 export default class GoogleLoginButton extends React.Component {
   static propTypes = {
     onLoginFinished: PropTypes.func.isRequired
   }
 
-  componentDidMount() {
+  componentDidMount () {
     GoogleSignin.configure({
       iosClientId: IOS_GOOGLE_CLIENT_ID,
       webClientId: WEB_GOOGLE_CLIENT_ID
