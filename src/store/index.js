9bd41f4b99870fab766b27c1cce82b6b2d6bcf87
Lawrence Wang <l@lw.io>
2017-06-13 16:08:56 -0700

    add remote-redux-devtools

--- src/store/index.js
+++ src/store/index.js
@@ -4,22 +4,25 @@ import rootReducer from '../reducer'
 import promiseMiddleware from 'redux-promise'
 import apiMiddleware from './middleware/api'
 import graphQLMiddleware from './middleware/graphQL'
+import extractModelMiddleware from './middleware/extractModel'
 import createLogger from 'redux-logger'
 import { AsyncStorage } from 'react-native'
 import { PERSISTED_STATE_KEY } from '../reducer/persistence'
 import orm from './models'
+import { composeWithDevTools } from 'remote-redux-devtools'
 
 const middleware = compact([
   graphQLMiddleware,
   apiMiddleware,
+  extractModelMiddleware,
   promiseMiddleware,
   __DEV__ && createLogger({collapsed: true})
 ])
 
 export default function getStore () {
   return getInitialState().then(initialState => {
-    const store = createStore(rootReducer, initialState,
-      applyMiddleware(...middleware))
+    const composedMiddleware = composeWithDevTools(applyMiddleware(...middleware))
+    const store = createStore(rootReducer, initialState, composedMiddleware)
 
     // Enable Webpack hot module replacement for reducers
     if (module.hot) {
@@ -34,6 +37,9 @@ export default function getStore () {
 }
 
 function getInitialState () {
+  return Promise.resolve(getEmptyState())
+  // for dev
+  // eslint-disable-next-line no-unreachable
   return AsyncStorage.getItem(PERSISTED_STATE_KEY)
   .then(state => state ? JSON.parse(state) : getEmptyState())
 }
