ee206ee17d24ae830032f894487c56682d6d5e7e
Rich Churcher <rich.churcher@gmail.com>
2017-06-14 06:59:21 +1200

    Use src, and update logger import method

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import rootReducer from '../reducer'
 import promiseMiddleware from 'redux-promise'
 import apiMiddleware from './middleware/api'
 import graphQLMiddleware from './middleware/graphQL'
-import createLogger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import { AsyncStorage } from 'react-native'
 import { PERSISTED_STATE_KEY } from '../reducer/persistence'
 
