01b3af82069e5b513c7e3d94322dfe98012b1f41
Lawrence Wang <l@lw.io>
2017-06-13 16:25:53 -0700

    remove old user data when logging out

--- src/reducer/ormReducer.js
+++ src/reducer/ormReducer.js
@@ -1,4 +1,5 @@
 import { EXTRACT_MODEL } from '../store/constants'
+import { LOGOUT } from '../components/Login/actions'
 import orm from '../store/models'
 import ModelExtractor from './ModelExtractor'
 
@@ -8,6 +9,11 @@ export default function ormReducer (state = {}, action) {
   if (error) return state
 
   switch (type) {
+    case LOGOUT:
+      const me = session.Me.first()
+      me.memberships.delete()
+      me.delete()
+      break
     case EXTRACT_MODEL:
       ModelExtractor.addAll({
         session,
