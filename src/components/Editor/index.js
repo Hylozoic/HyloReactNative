4231810b4359827ee14988678fdf907a28158e0a
Lawrence Wang <l@lw.io>
2017-06-27 16:28:30 -0700

    start wiring up mention and topic buttons in editor toolbar

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import { KeyboardAvoidingView, View } from 'react-native'
-import { RichTextEditor, RichTextToolbar } from 'react-native-zss-rich-text-editor'
+import { KeyboardAvoidingView, Text, View } from 'react-native'
+import { RichTextEditor, RichTextToolbar, actions } from 'react-native-zss-rich-text-editor'
+import Icon from '../Icon'
+
+const INSERT_MENTION = 'Hylo/INSERT_MENTION'
+const INSERT_TOPIC = 'Hylo/INSERT_TOPIC'
 
 export default class Editor extends React.Component {
   getContentAsync () {
     return this.editor.getContentHtml()
+  }
+
+  insertMention () {
+    console.log('insertMention')
+  }
+
+  insertTopic () {
+    console.log('insertTopic')
   }
 
   render () {
@@ -16,7 +28,29 @@ export default class Editor extends React.Component {
           titlePlaceholder='title'
           contentPlaceholder='details' />
       </View>
-      <RichTextToolbar getEditor={() => this.editor} />
+      <RichTextToolbar getEditor={() => this.editor}
+        actions={[
+          actions.setBold,
+          actions.setItalic,
+          actions.insertLink,
+          INSERT_MENTION,
+          INSERT_TOPIC
+        ]}
+        renderIcon={(action, selected) => {
+          switch (action) {
+            case INSERT_MENTION:
+            case INSERT_TOPIC:
+              return <ToolbarIcon action={action} />
+          }
+        }}
+        onPressCustomAction={action => {
+          switch (action) {
+            case INSERT_MENTION:
+              return this.insertMention()
+            case INSERT_TOPIC:
+              return this.insertTopic()
+          }
+        }}/>
     </KeyboardAvoidingView>
   }
 }
@@ -29,5 +63,37 @@ const styles = {
   },
   wrapper: {
     flex: 1
+  }
+}
+
+function ToolbarIcon ({ action }) {
+  let content
+  switch (action) {
+    case INSERT_MENTION:
+      content = <Text style={iconStyles.mention}>@</Text>
+      break
+    case INSERT_TOPIC:
+      content = <Icon name='Topics' style={iconStyles.topic} />
+      break
+  }
+
+  return <View style={iconStyles.container}>
+    {content}
+  </View>
+}
+
+const iconStyles = {
+  container: {
+    alignItems: 'center'
+  },
+  mention: {
+    color: '#808080',
+    fontSize: 22,
+    fontWeight: '200',
+    paddingBottom: 2
+  },
+  topic: {
+    color: '#999',
+    fontSize: 22
   }
 }
