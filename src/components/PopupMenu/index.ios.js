f470b8a642a3d51ba7c2232041527530e02fc265
Lawrence Wang <l@lw.io>
2017-06-28 16:25:15 -0700

    implement iOS side of PopupMenu

--- src/components/PopupMenu/index.ios.js
+++ src/components/PopupMenu/index.ios.js
@@ -1,3 +1,16 @@
-import Component from './PopupMenu'
+import { ActionSheetIOS, TouchableOpacity } from 'react-native'
+import React from 'react'
 
-export default Component
+export default function ({ actions, onSelect, destructiveButtonIndex, children }) {
+  const onPress = () => {
+    const cancelButtonIndex = actions.length
+    ActionSheetIOS.showActionSheetWithOptions({
+      options: actions.concat(['Cancel']),
+      destructiveButtonIndex,
+      cancelButtonIndex: actions.length
+    }, index => index !== cancelButtonIndex && onSelect(index))
+  }
+  return <TouchableOpacity onPress={onPress}>
+    {children}
+  </TouchableOpacity>
+}
