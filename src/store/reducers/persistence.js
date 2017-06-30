0a2928e6b84f669819538fdb020df50a8e537050
Lawrence Wang <l@lw.io>
2017-06-30 15:02:04 -0700

    debounce persistence

--- src/store/reducers/persistence.js
+++ src/store/reducers/persistence.js
@@ -1,16 +1,19 @@
 import { AsyncStorage } from 'react-native'
 export const PERSISTED_STATE_KEY = 'persisted-app-state-v2'
+import { debounce } from 'lodash'
 
 export function persist (reducer) {
-  return function (state, action) {
+  return (state, action) => {
     const nextState = reducer(state, action)
-    if (nextState !== state) {
-      const serialized = JSON.stringify(nextState)
-      const startTime = Date.now()
-      AsyncStorage.setItem(PERSISTED_STATE_KEY, serialized, () => {
-        console.log(`persisted state in ${Date.now() - startTime}ms`)
-      })
-    }
+    if (nextState !== state) save(nextState)
     return nextState
   }
 }
+
+const save = debounce(state => {
+  const serialized = JSON.stringify(state)
+  const startTime = Date.now()
+  AsyncStorage.setItem(PERSISTED_STATE_KEY, serialized, () => {
+    console.log(`persisted state in ${Date.now() - startTime}ms`)
+  })
+}, 2000)
