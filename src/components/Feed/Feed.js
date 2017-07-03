c9da6466dc87455174abe153e94466054b79ed16
Lawrence Wang <l@lw.io>
2017-07-03 13:12:49 -0700

    add workaround for race condition when loading Home tab
    Feed starts loading before fetchCurrentUser finishes, so it starts
    fetching data for All Communities, but then fetchCurrentUser finishes
    and Feed changes to focus on one community, but doesn’t re-fetch data

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import FeedList from '../FeedList'
+import Loading from '../Loading'
 import FeedBanner from '../FeedBanner'
 import styles from './Feed.styles'
 
 export default class Feed extends Component {
   render () {
     const { community, currentUser, newPost } = this.props
+
+    if (!currentUser) return <Loading style={{flex: 1}} />
 
     return <View style={styles.container}>
       <FeedList
--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-export default function Loading () {
-  return <View>
-    <Text>Loading...</Text>
+export default function Loading ({ style }) {
+  return <View style={[style, styles.container]}>
+    <Text style={styles.text}>Loading...</Text>
   </View>
+}
+
+const styles = {
+  container: {
+    justifyContent: 'space-around'
+  },
+  text: {
+    textAlign: 'center'
+  }
 }
