bca3dd0d3dd97f8c28b48440434d3b21e004ed8e
Robbie Carlton <robbie.carlton@gmail.com>
2017-07-06 15:42:34 -0600

    tests

---
+++ src/components/PostDetails/CommentEditor/CommentEditor.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactShallowRenderer from 'react-test-renderer/shallow'
+import CommentEditor from './CommentEditor'
+
+/*
+it('renders correctly', () => {
+  const renderer = new ReactShallowRenderer()
+  renderer.render(<CommentEditor
+    content={'lalala'} />)
+  const actual = renderer.getRenderOutput()
+
+  expect(actual).toMatchSnapshot()
+})
+*/
--- src/components/PostDetails/PostDetails.test.js
+++ src/components/PostDetails/PostDetails.test.js
@@ -1,39 +1,66 @@
 import React from 'react'
 import ReactShallowRenderer from 'react-test-renderer/shallow'
-import PostDetails from './PostDetails'
+import PostDetails, { CommentPrompt } from './PostDetails'
 
-it('renders correctly', () => {
-  const post = {
-    id: '91',
-    creator: {
-      id: '77',
-      name: 'Houdini'
-    },
-    communities: [{slug: 'foom'}],
-    createdAt: '2017-05-19T23:24:58Z',
-    imageUrl: 'foom.png',
-    title: 'Hi',
-    details: 'Lo',
-    linkPreview: {
-      id: '34'
-    },
-    commenters: [{id: 9}, {id: 7}],
-    commentsTotal: 12,
-    votesTotal: 8,
-    myVote: true,
-    type: 'request'
-  }
-  const currentUser = {
-    id: 123
-  }
+const post = {
+  id: '91',
+  creator: {
+    id: '77',
+    name: 'Houdini'
+  },
+  communities: [{slug: 'foom'}],
+  createdAt: '2017-05-19T23:24:58Z',
+  imageUrl: 'foom.png',
+  title: 'Hi',
+  details: 'Lo',
+  linkPreview: {
+    id: '34'
+  },
+  commenters: [{id: 9}, {id: 7}],
+  commentsTotal: 12,
+  votesTotal: 8,
+  myVote: true,
+  type: 'request'
+}
+const currentUser = {
+  id: 123,
+  avatarUrl: 'me.png',
+  firstName: () => 'Joe'
+}
 
-  const renderer = new ReactShallowRenderer()
-  renderer.render(<PostDetails
-    post={post}
-    currentUser={currentUser}
-    editPost={() => {}}
-    pending />)
-  const actual = renderer.getRenderOutput()
+describe('PostDetails', () => {
+  it('renders correctly', () => {
+    const renderer = new ReactShallowRenderer()
+    renderer.render(<PostDetails
+      post={post}
+      currentUser={currentUser}
+      editPost={() => {}}
+      pending />)
+    const actual = renderer.getRenderOutput()
 
-  expect(actual).toMatchSnapshot()
+    expect(actual).toMatchSnapshot()
+  })
+})
+
+describe('CommentPrompt', () => {
+  it('renders correctly', () => {
+    const renderer = new ReactShallowRenderer()
+    renderer.render(<CommentPrompt
+      currentUser={currentUser}
+      newComment={() => {}} />)
+    const actual = renderer.getRenderOutput()
+
+    expect(actual).toMatchSnapshot()
+  })
+
+  it('renders with comment text', () => {
+    const renderer = new ReactShallowRenderer()
+    renderer.render(<CommentPrompt
+      currentUser={currentUser}
+      newComment={() => {}}
+      commentEdit='A long enough comment that it will be truncated' />)
+    const actual = renderer.getRenderOutput()
+
+    expect(actual).toMatchSnapshot()
+  })
 })
