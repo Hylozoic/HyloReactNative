f4aa2738382b6bd5dae3fffd2177ac7c4fc2fcbc
Rich Churcher <rich.churcher@gmail.com>
2017-07-08 17:04:25 +1200

    Update tests

--- src/components/MemberProfile/MemberProfile.test.js
+++ src/components/MemberProfile/MemberProfile.test.js
@@ -6,7 +6,7 @@ import MemberProfile from './MemberProfile'
 it('matches the last snapshot', () => {
 
   const renderer = new ReactShallowRenderer()
-  renderer.render(<MemberProfile id={1} />)
+  renderer.render(<MemberProfile navigation={{ state: { params: {} } }} />)
   const actual = renderer.getRenderOutput()
 
   expect(actual).toMatchSnapshot()
