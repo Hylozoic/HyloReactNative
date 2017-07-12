7db12a99c0d622395d6679fb617e909cd90d7d84
Lawrence Wang <l@lw.io>
2017-07-12 16:48:05 -0700

    set current community to last-viewed one on login

--- src/store/reducers/currentCommunity.js
+++ src/store/reducers/currentCommunity.js
@@ -1,6 +1,17 @@
 import { CHANGE_COMMUNITY } from 'store/constants'
+import { FETCH_CURRENT_USER } from '../actions/fetchCurrentUser'
+import { maxBy } from 'lodash'
 
 export default function currentCommunity (state = null, action) {
-  if (action.type === CHANGE_COMMUNITY) return action.payload
+  const { error, type, payload } = action
+
+  if (type === CHANGE_COMMUNITY) return payload
+
+  if (type === FETCH_CURRENT_USER && !error) {
+    const lastViewedMembership = maxBy(payload.data.me.memberships,
+      ms => new Date(ms.lastViewedAt))
+    return lastViewedMembership.community.id
+  }
+
   return state
 }
