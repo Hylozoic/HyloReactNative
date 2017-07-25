29d5f0280426519f08a4bac6fba6d8f3680ec5bf
Lawrence Wang <l@lw.io>
2017-07-25 09:15:52 -0700

    add sails.io.js (#34)
    MBL-42 add sails.io.js

---
+++ src/components/SocketListener/SocketListener.store.js
@@ -0,0 +1,110 @@
+const MODULE_NAME = 'SocketListener'
+export const RECEIVE_MESSAGE = `${MODULE_NAME}/RECEIVE_MESSAGE`
+export const RECEIVE_COMMENT = `${MODULE_NAME}/RECEIVE_COMMENT`
+export const RECEIVE_POST = `${MODULE_NAME}/RECEIVE_POST`
+export const RECEIVE_THREAD = `${MODULE_NAME}/RECEIVE_THREAD`
+export const RECEIVE_NOTIFICATION = `${MODULE_NAME}/RECEIVE_NOTIFICATION`
+export const HANDLE_EVENT = `${MODULE_NAME}/HANDLE_EVENT`
+
+export function receiveMessage (message, opts = {}) {
+  return {
+    type: RECEIVE_MESSAGE,
+    payload: {
+      data: {
+        message
+      }
+    },
+    meta: {
+      extractModel: 'Message',
+      bumpUnreadCount: opts.bumpUnreadCount
+    }
+  }
+}
+
+export function receiveComment (comment, opts = {}) {
+  return {
+    type: RECEIVE_COMMENT,
+    payload: {
+      data: {
+        comment
+      }
+    },
+    meta: {
+      extractModel: 'Comment'
+    }
+  }
+}
+
+export function receiveThread (thread) {
+  return {
+    type: RECEIVE_THREAD,
+    payload: {
+      data: {
+        thread
+      }
+    },
+    meta: {
+      extractModel: 'MessageThread'
+    }
+  }
+}
+
+export function receivePost (data, communityId) {
+  return {
+    type: RECEIVE_POST,
+    payload: {
+      topics: data.tags,
+      creatorId: data.creatorId,
+      communityId
+    }
+  }
+}
+
+export function receiveNotification (notification) {
+  return {
+    type: RECEIVE_NOTIFICATION,
+    payload: {
+      data: {
+        notification
+      }
+    },
+    meta: {
+      extractModel: 'Notification'
+    }
+  }
+}
+
+export function handleEvent (name, value) {
+  return {
+    type: HANDLE_EVENT,
+    payload: {name, value}
+  }
+}
+
+export default function reducer (state = {}, action) {
+  if (action.type === HANDLE_EVENT) {
+    const events = state.events || {}
+    const { name, value } = action.payload
+    const newState = {
+      ...state,
+      events: {
+        ...events,
+        [name]: {
+          count: (events[name] ? events[name].count : 0) + 1,
+          latest: new Date(),
+          value: value && value.message ? value.message : value
+        }
+      }
+    }
+
+    // toggle connected boolean status
+    if (['connect', 'reconnect'].includes(name)) {
+      newState.connected = true
+    } else if (name === 'disconnect') {
+      newState.connected = false
+    }
+
+    return newState
+  }
+  return state
+}
