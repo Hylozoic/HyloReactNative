fe23199e7e44c85c7d3d796316173cde82ba593b
Robbie Carlton <robbie.carlton@gmail.com>
2017-07-25 16:46:03 -0700

    add test

--- src/components/MemberProfile/MemberProfile.test.js
+++ src/components/MemberProfile/MemberProfile.test.js
@@ -4,7 +4,6 @@ import ReactShallowRenderer from 'react-test-renderer/shallow'
 import MemberProfile from './MemberProfile'
 
 it('matches the last snapshot', () => {
-
   const renderer = new ReactShallowRenderer()
   renderer.render(<MemberProfile navigation={{ state: { params: {} } }} />)
   const actual = renderer.getRenderOutput()
