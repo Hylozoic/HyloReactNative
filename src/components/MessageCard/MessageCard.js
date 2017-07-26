959af86aeb793df1bb99ec3f3eb83b2e8e539649
Rich Churcher <rich.churcher@gmail.com>
2017-07-27 08:17:42 +1200

    Probably just need a dumb component

--- src/components/MessageCard/MessageCard.js
+++ src/components/MessageCard/MessageCard.js
@@ -6,29 +6,26 @@ import Avatar from '../Avatar'
 
 import styles from './MessageCard.style'
 
-export default class MessageCard extends React.Component {
-  static propTypes = {
-    message: shape({
-      id: any,
-      createdAt: string,
-      text: string,
-      creator: shape({
-        id: any,
-        name: string,
-        avatarUrl: string
-      })
-    })
-  }
-
-  render () {
-    const { message } = this.props
-    return <View style={styles.container}>
-      <Avatar avatarUrl={message.creator.avatarUrl}/>
-      <View style={styles.body}>
-        <Text style={styles.name}>{message.creator.name}</Text>
-        <Text style={styles.text}>{message.text}</Text>
-        <Text style={styles.date}>{message.createdAt}</Text>
-      </View>
+export default function MessageCard ({ message }) {
+  return <View style={styles.container}>
+    <Avatar avatarUrl={message.creator.avatarUrl}/>
+    <View style={styles.body}>
+      <Text style={styles.name}>{message.creator.name}</Text>
+      <Text style={styles.text}>{message.text}</Text>
+      <Text style={styles.date}>{message.createdAt}</Text>
     </View>
-  }
+  </View>
+}
+
+MessageCard.propTypes = {
+  message: shape({
+    id: any,
+    createdAt: string,
+    text: string,
+    creator: shape({
+      id: any,
+      name: string,
+      avatarUrl: string
+    })
+  })
 }
