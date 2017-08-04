6d4d82ac8cd4307d31107107ef99e93f212639e5
Robbie Carlton <robbie.carlton@gmail.com>
2017-08-04 16:40:55 -0600

    (wip) wrestling with aws

--- src/components/ImagePicker/ImagePicker.js
+++ src/components/ImagePicker/ImagePicker.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Text, TouchableOpacity, View, Image } from 'react-native'
 import RNImagePicker from 'react-native-image-picker'
+import { AWS_S3_BUCKET } from 'react-native-dotenv'
+import aws from 'aws-sdk'
 
 export default class ImagePicker extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
 
   showPicker () {
     const { title = 'Choose an image' } = this.props
@@ -22,28 +29,38 @@ export default class ImagePicker extends Component {
         console.log('User cancelled image picker')
       } else if (response.error) {
         console.log('ImagePicker Error: ', response.error)
-      } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton)
       } else {
-        let source = { uri: response.uri }
-
-        // You can also display the image using data:
-        // let source = { uri: 'data:image/jpeg;base64,' + response.data };
-
-        console.log('source', source)
-
-        this.setState({
-          avatarSource: source
+        this.setState({source: {uri: response.uri}})
+        const s3 = new aws.S3()
+        const key = '/testuploads/' + response.uri
+        const body = 'data:image/png;base64,' + response.data
+        return s3.upload({
+          Bucket: AWS_S3_BUCKET,
+          ACL: 'public-read',
+          ContentType: 'image/png',
+          Key: key,
+          Body: body
+        }).promise()
+        .then(result => {
+          console.log('s3 came back with', result)
         })
       }
     })
   }
 
   render () {
+    const { source } = this.state
+
     return <View>
       <TouchableOpacity onPress={() => this.showPicker()}>
         <Text>Image Picker</Text>
+        {!!source && <Image source={source} style={imageStyle}/>}
       </TouchableOpacity>
     </View>
   }
+}
+
+const imageStyle = {
+  width: 256,
+  height: 256
 }
