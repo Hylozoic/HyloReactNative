1729ea3576f606488b77a1f49e4b5959731a4bdf
Ray Marceau <razor2n4@gmail.com>
2017-08-08 22:09:58 -0700

    removes the yellow warning box on android

--- index.android.js
+++ index.android.js
@@ -3,4 +3,9 @@ import { AppRegistry } from 'react-native'
 
 AppRegistry.registerComponent('HyloReactNative', () => RootView)
 
+// Removes the yellow warning box related to https://github.com/facebook/react-native/issues/12981
+console.ignoredYellowBox = [
+  'Setting a timer'
+]
+
 export default RootView
