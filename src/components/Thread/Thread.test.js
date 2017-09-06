4e292087b054f54d28d6e8293d677a564daa3665
Rich Churcher <rich.churcher@gmail.com>
2017-09-06 17:10:06 +1200

    Mocking, horrible horrible mocking.

--- src/components/Thread/Thread.test.js
+++ src/components/Thread/Thread.test.js
@@ -1,7 +1,7 @@
 import 'react-native'
 import React from 'react'
 import ReactShallowRenderer from 'react-test-renderer/shallow'
-
+import ReactTestRenderer from 'react-test-renderer'
 import Thread from './Thread'
 
 it('matches the last snapshot', () => {
@@ -12,22 +12,22 @@ it('matches the last snapshot', () => {
     fetchMessages: () => {},
     messages: [
       {
-        id: "56025",
-        createdAt: "Mon Aug 14 2017 18:58:37 GMT+1200 (NZST)",
-        text: "Flargle argle",
-        creator: { id: "86894" }
+        id: '56025',
+        createdAt: 'Mon Aug 14 2017 18:58:37 GMT+1200 (NZST)',
+        text: 'Flargle argle',
+        creator: { id: '86894', name: 'Yup', avatarUrl: 'http://yup.com' }
       },
       {
-        id: "56024",
-        createdAt: "Mon Aug 14 2017 17:27:42 GMT+1200 (NZST)",
-        text: "Argle bargle",
-        creator: { id: "86895" }
+        id: '56024',
+        createdAt: 'Mon Aug 14 2017 17:27:42 GMT+1200 (NZST)',
+        text: 'Argle bargle',
+        creator: { id: '86895', name: 'Nope', avatarUrl: 'http://nope.com' }
       },
       {
-        id: "56023",
-        createdAt: "Mon Aug 14 2017 15:24:36 GMT+1200 (NZST)",
-        text: "Bargle wargle",
-        creator: { id: "86895" }
+        id: '56023',
+        createdAt: 'Mon Aug 14 2017 15:24:36 GMT+1200 (NZST)',
+        text: 'Bargle wargle',
+        creator: { id: '86896', name: 'Maybe', avatarUrl: 'http://maybe.com' }
       }
     ],
     reconnectFetchMessages: () => {},
@@ -37,4 +37,84 @@ it('matches the last snapshot', () => {
 
   renderer.render(<Thread { ...props } />)
   expect(renderer.getRenderOutput()).toMatchSnapshot()
+})
+
+it('uses the new API', () => {
+  const props = {
+    createMessage: () => {},
+    currentUserId: '1',
+    fetchMessages: () => {},
+    messages: [
+      {
+        id: '56025',
+        createdAt: 'Mon Aug 14 2017 18:58:37 GMT+1200 (NZST)',
+        text: 'Flargle argle',
+        creator: { id: '86894', name: 'Yup', avatarUrl: 'http://yup.com' }
+      },
+      {
+        id: '56024',
+        createdAt: 'Mon Aug 14 2017 17:27:42 GMT+1200 (NZST)',
+        text: 'Argle bargle',
+        creator: { id: '86895', name: 'Nope', avatarUrl: 'http://nope.com' }
+      },
+      {
+        id: '56023',
+        createdAt: 'Mon Aug 14 2017 15:24:36 GMT+1200 (NZST)',
+        text: 'Bargle wargle',
+        creator: { id: '86896', name: 'Maybe', avatarUrl: 'http://maybe.com' }
+      }
+    ],
+    reconnectFetchMessages: () => {},
+    setTitle: () => {},
+    updateThreadReadTime: () => {}
+  }
+
+  const { root } = ReactTestRenderer.create(<Thread { ...props } />)
+})
+
+// Mocks get hoisted. Currently cannot get this to work with manual __mocks__
+// folders and our Redux container setup... pointers welcome!
+//
+// Workaround for no refs in react-test-renderer, extrapolated from
+// https://github.com/facebook/react-native/issues/15000#issuecomment-315250887
+// If used more than once, this could be extracted to a more general location
+jest.mock('FlatList', () => {
+  const React = require('React')
+  class FlatList extends React.Component {
+    _listRef = { scrollToOffset: jest.fn() }
+    scrollToOffset = jest.fn()
+
+    render () {
+      return React.createElement('FlatList', this.props, this.props.children)
+    }
+  }
+  return FlatList
+})
+
+jest.mock('NetInfo', () => ({
+  isConnected: {
+    addEventListener: jest.fn()
+  }
+}))
+
+// Can actually get away with `jest.mock('../SocketSubscriber')` here, but it
+// generates warnings because `undefined` isn't exactly a React component...
+jest.mock('../SocketSubscriber', () => {
+  const React = require('React')
+  class SocketSubscriber extends React.Component {
+    render () {
+      return React.createElement('SocketSubscriber', this.props, this.props.children)
+    }
+  }
+  return SocketSubscriber
+})
+
+jest.mock('../PeopleTyping', () => {
+  const React = require('React')
+  class PeopleTyping extends React.Component {
+    render () {
+      return React.createElement('PeopleTyping', this.props, this.props.children)
+    }
+  }
+  return PeopleTyping
 })
