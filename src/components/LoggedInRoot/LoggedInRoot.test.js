62631ee40b0bd095747e3e6c94c2656899652885
Lawrence Wang <l@lw.io>
2017-09-26 12:10:41 -0700

    clean up setting of API host

--- src/components/LoggedInRoot/LoggedInRoot.test.js
+++ src/components/LoggedInRoot/LoggedInRoot.test.js
@@ -2,7 +2,7 @@ import ReactShallowRenderer from 'react-test-renderer/shallow'
 import LoggedInRoot from './LoggedInRoot'
 import React from 'react'
 
-jest.mock('react-native-google-signin', () => {})
+jest.mock('react-native-google-signin')
 jest.mock('react-native-zss-rich-text-editor')
 jest.mock('react-native-onesignal', () => ({
   getPermissionSubscriptionState: jest.fn(() => Promise.resolve({userId: 5}))
--- src/util/fetchJSON.js
+++ src/util/fetchJSON.js
@@ -1,18 +1,9 @@
 import { setSessionCookie } from './session'
-import { isIOS } from 'util/platform'
-
-export const HOST =
-  (__DEV__ && isIOS &&
-    (process.env.IOS_API_HOST || process.env.IOS_EMULATOR_API_HOST)) ||
-  (__DEV__ && !isIOS &&
-    (process.env.ANDROID_API_HOST || process.env.ANDROID_EMULATOR_API_HOST)) ||
-  process.env.API_HOST
-
-console.log(`API host: ${HOST}`)
+import apiHost from './apiHost'
 
 export default function fetchJSON (path, params, options = {}) {
   const { host, method } = options
-  const url = (host || HOST) + path
+  const url = (host || apiHost) + path
   return fetch(url, {
     method: method || 'get',
     headers: {
