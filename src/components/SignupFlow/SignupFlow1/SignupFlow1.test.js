1619e575b473f70adccb7513c7a25e05591f2f38
Robbie Carlton <robbie.carlton@gmail.com>
2017-09-28 16:34:48 -0600

    more Signup1 tests

--- src/components/SignupFlow/SignupFlow1/SignupFlow1.test.js
+++ src/components/SignupFlow/SignupFlow1/SignupFlow1.test.js
@@ -1,6 +1,7 @@
 import 'react-native'
 import React from 'react'
 import ReactShallowRenderer from 'react-test-renderer/shallow'
+import ReactTestRenderer from 'react-test-renderer'
 import SignupFlow1 from './SignupFlow1'
 
 jest.mock('../../KeyboardFriendlyView', () => 'KeyboardFriendlyView')
@@ -35,4 +36,79 @@ it('hides password, and changes button when pending ', () => {
   const actual = renderer.getRenderOutput()
 
   expect(actual).toMatchSnapshot()
+})
+
+it('calls functions on mount', () => {
+  const props = {
+    currentUser: {id: 1},
+    fetchCurrentUser: jest.fn(),
+    loadUserSettings: jest.fn()
+  }
+  ReactTestRenderer.create(<SignupFlow1 {...props} />)
+  expect(props.fetchCurrentUser).toHaveBeenCalled()
+  expect(props.loadUserSettings).toHaveBeenCalled()
+})
+
+it('loads user settings and updates errors on update when appropriate', () => {
+  const props = {
+    currentUser: {id: 1},
+    errors: {
+      email: 'one error'
+    },
+    loadUserSettings: jest.fn(),
+    fetchCurrentUser: () => {}
+  }
+  const instance = ReactTestRenderer.create(<SignupFlow1 {...props} />).root.instance
+  props.loadUserSettings.mockClear()
+
+  instance.componentDidUpdate(props)
+  expect(props.loadUserSettings).not.toHaveBeenCalled()
+  expect(instance.state.errors).toMatchSnapshot()
+
+  const differentProps = {
+    currentUser: {id: 2},
+    errors: {
+      email: 'old error'
+    }
+  }
+
+  instance.componentDidUpdate(differentProps)
+  expect(props.loadUserSettings).toHaveBeenCalled()
+  expect(instance.state.errors).toMatchSnapshot()
+})
+
+it('validates its fields', () => {
+  const props = {
+    name: '',
+    email: 'ra',
+    password: 'rarara',
+    showPasswordField: true,
+    fetchCurrentUser: () => {},
+    loadUserSettings: () => {}
+  }
+
+  const instance = ReactTestRenderer.create(<SignupFlow1 {...props} />).root.instance
+  expect(instance.validate()).toBeFalsy()
+  expect(instance.state).toMatchSnapshot()
+})
+
+it('clears errors when updating fields', () => {
+  const props = {
+    changeSetting: jest.fn(),
+    fetchCurrentUser: () => {},
+    loadUserSettings: () => {}
+  }
+
+  const instance = ReactTestRenderer.create(<SignupFlow1 {...props} />).root.instance
+  instance.setState({
+    errors: {
+      email: 'yes',
+      name: 'yes'
+    }
+  })
+  instance.updateField('email', 'new@email')
+  expect(instance.state.errors).toEqual({
+    email: null,
+    name: 'yes'
+  })
 })
