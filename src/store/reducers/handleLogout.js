493e2eca73a21758e072f723fc7dbf433a1238eb
Lawrence Wang <l@lw.io>
2017-09-29 15:13:47 -0700

    don't persist state after logout

--- src/store/reducers/handleLogout.js
+++ src/store/reducers/handleLogout.js
@@ -1,8 +1,10 @@
 import { getEmptyState } from '../index'
 import { LOGOUT } from '../../components/Login/actions'
+import { reset } from './persistence'
 
 export default function (state, action) {
   if (action.type === LOGOUT) {
+    reset() // this is an async action with side effects!
     return {
       ...getEmptyState(),
       session: {
