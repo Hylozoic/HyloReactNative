43d62001b24589fb0ec67ccc878fabee4e10d906
Ray Marceau <razor2n4@gmail.com>
2017-10-02 13:53:23 -0700

    onSelect tests for

--- src/components/PopupMenuButton/PopupMenuButton.android.test.js
+++ src/components/PopupMenuButton/PopupMenuButton.android.test.js
@@ -5,14 +5,57 @@ import TestRenderer from 'react-test-renderer'
 import { TouchableOpacity } from 'react-native'
 
 it('matches the last snapshot', () => {
-  const onSelect = jest.fn()
   const action1 = jest.fn()
   const action2 = jest.fn()
+  const actions = [['Action 1', action1], ['Action 2', action2]]
 
-  const renderer = TestRenderer.create(<PopupMenuButton actions={[['Action 1', action1], ['Action 2', action2]]} onSelect={onSelect}
-  destructiveButtonIndex={0}>
+  const props = {
+    actions: actions.map(x => x[0]),
+    onSelect: jest.fn(),
+    destructiveButtonIndex: 0
+  }
+  const renderer = TestRenderer.create(<PopupMenuButton {...props}>
     Hello World
   </PopupMenuButton>)
 
   expect(renderer).toMatchSnapshot()
+})
+
+it('calls our onSelect with the right index', () => {
+  const action1 = jest.fn()
+  const action2 = jest.fn()
+  const actions = [['Action 1', action1], ['Action 2', action2]]
+
+  const props = {
+    actions: actions.map(x => x[0]),
+    onSelect: jest.fn(),
+    destructiveButtonIndex: 0
+  }
+
+  const instance = TestRenderer.create(<PopupMenuButton {...props}>
+    Hello World
+  </PopupMenuButton>).root.instance
+
+  instance.onSelect('itemSelected', 1)
+  expect(props.onSelect).toHaveBeenCalledWith(1)
+})
+
+it('doesnt call onSelect if clicked away', () => {
+  const action1 = jest.fn()
+  const action2 = jest.fn()
+  const actions = [['Action 1', action1], ['Action 2', action2]]
+
+  const props = {
+    actions: actions.map(x => x[0]),
+    onSelect: jest.fn(),
+    destructiveButtonIndex: 0
+  }
+
+  const instance = TestRenderer.create(<PopupMenuButton {...props}>
+    Hello World
+  </PopupMenuButton>).root.instance
+
+  instance.onSelect('dismissed', 1)
+  expect(props.onSelect).not.toHaveBeenCalled()
+
 })
