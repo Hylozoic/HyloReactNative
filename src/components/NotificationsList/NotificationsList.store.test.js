f5ca3fd0476e4705ce29e4480e525f8045edca0c
Rich Churcher <rich.churcher@gmail.com>
2017-10-04 10:18:07 +1300

    Action creator snapshots

--- src/components/NotificationsList/NotificationsList.store.test.js
+++ src/components/NotificationsList/NotificationsList.store.test.js
@@ -1,86 +1,17 @@
-import orm from 'store/models'
-import { FETCH_NOTIFICATIONS, getNotifications } from './NotificationsList.store'
-import { buildKey } from '../../store/reducers/queryResults'
+import * as store from './NotificationsList.store'
 
-const session = orm.session(orm.getEmptyState())
+describe('NotificationsList.store', () => {
+  describe('action creators', () => {
+    it('matches the last snapshot from fetchNotifications', () => {
+      expect(store.fetchNotifications(20, 0)).toMatchSnapshot()
+    })
 
-// const specs = [
-//   {
-//     modelName: 'Person',
-//     values: {
-//       id: 1,
-//       name: 'First Person'
-//     }
-//   },
-//   {
-//     modelName: 'Person',
-//     values: {
-//       id: 2,
-//       name: 'Second Person'
-//     }
-//   },
-//   {
-//     modelName: 'Person',
-//     values: {
-//       id: 3,
-//       name: 'Third Person'
-//     }
-//   },
-//   {
-//     modelName: 'MessageThread',
-//     values: {
-//       id: 1,
-//       unreadCount: 1,
-//       participants: [1, 2]
-//     }
-//   },
-//   {
-//     modelName: 'MessageThread',
-//     values: {
-//       id: 2,
-//       unreadCount: 1,
-//       participants: [2, 3]
-//     }
-//   },
-//   {
-//     modelName: 'Message',
-//     values: {
-//       id: 1,
-//       text: 'First message',
-//       messageThread: 1
-//     }
-//   },
-//   {
-//     modelName: 'Message',
-//     values: {
-//       id: 2,
-//       text: 'Second message',
-//       messageThread: 1
-//     }
-//   },
-//   {
-//     modelName: 'Message',
-//     values: {
-//       id: 3,
-//       text: 'Third message',
-//       messageThread: 2
-//     }
-//   }
-// ]
-//
-// specs.forEach(spec => session[spec.modelName].create(spec.values))
-//
-// it('gets threads and denormalizes messages and participants', () => {
-//   const state = {
-//     orm: session.state,
-//     pending: {},
-//     queryResults: {
-//       [buildKey(FETCH_THREADS, {slug: 'foo'})]: {
-//         ids: ['1', '3', '2'],
-//         hasMore: true
-//       }
-//     }
-//   }
-//
-//   expect(getThreads(state, null)).toMatchSnapshot()
-// })
+    it('matches the last snapshot from markActivityRead', () => {
+      expect(store.markActivityRead('1')).toMatchSnapshot()
+    })
+
+    it('matches the last snapshot from markAllActivitiesRead', () => {
+      expect(store.markAllActivitiesRead()).toMatchSnapshot()
+    })
+  })
+})
