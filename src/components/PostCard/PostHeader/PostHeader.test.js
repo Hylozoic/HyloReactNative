f22c1ec5e246350e219aaafc90c82fad6c955053
Ray Marceau <razor2n4@gmail.com>
2017-10-03 13:01:44 -0700

    more coverage

--- src/components/PostCard/PostHeader/PostHeader.test.js
+++ src/components/PostCard/PostHeader/PostHeader.test.js
@@ -1,7 +1,7 @@
 import 'react-native'
 import React from 'react'
 import ReactShallowRenderer from 'react-test-renderer/shallow'
-import PostHeader, { PostMenu } from './PostHeader'
+import PostHeader, { PostMenu, PostLabel } from './PostHeader'
 
 it('renders correctly with all=true, and no community or user', () => {
   const creator = {
@@ -75,6 +75,17 @@ it('renders correctly with no flagging', () => {
   const actual = renderer.getRenderOutput()
 
   expect(actual).toMatchSnapshot()
+})
+
+describe('PostLabel', () => {
+  it('renders', () => {
+    const renderer = new ReactShallowRenderer()
+    renderer.render(<PostLabel type={'request'} />)
+
+    const actual = renderer.getRenderOutput()
+
+    expect(actual).toMatchSnapshot()
+  })
 })
 
 describe('PostMenu', () => {
