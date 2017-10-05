8676abe4494b3f72bab0260593e0e9edd61eb3a2
Andy Gimma <andy@hylo.com>
2017-10-05 17:49:13 -0500

    Add tests

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -1,9 +1,15 @@
 import 'react-native'
 import React from 'react'
 import ReactShallowRenderer from 'react-test-renderer/shallow'
+import TestRenderer from 'react-test-renderer'
 import Login, { FormError } from './Login'
 
-jest.mock('react-native-google-signin', () => {})
+jest.mock('react-native-google-signin')
+jest.mock('NetInfo', () => ({
+  isConnected: {
+    addEventListener: jest.fn()
+  }
+}))
 
 describe('Login', () => {
   it('renders correctly', () => {
@@ -51,6 +57,48 @@ describe('Login', () => {
     />)
     const actual = renderer.getRenderOutput()
     expect(actual).toMatchSnapshot()
+  })
+
+  it('calls login successfully', () => {
+    const login = jest.fn()
+    const instance = TestRenderer.create(<Login
+      login={login}
+    />
+    ).root.instance
+
+    instance.login()
+    expect(login).toHaveBeenCalled()
+  })
+
+  it('calls validateEmail successfully', () => {
+    const validEmail = 'test@email.com'
+    const invalidEmail = 'test'
+    const instance = TestRenderer.create(<Login
+      error={''}
+     />
+    ).root.instance
+
+    instance.validateEmail(validEmail)
+    expect(instance.state.emailIsValid).toBeTruthy()
+
+    instance.validateEmail(invalidEmail)
+    expect(instance.state.emailIsValid).toBeFalsy()
+  })
+
+  it('calls setInput successfully', () => {
+    const emailKey = 'email'
+    const passwordKey = 'password'
+    const email = 'test@email.com'
+    const password = 'pass'
+    const instance = TestRenderer.create(<Login />
+    ).root.instance
+
+    instance.setInput(emailKey, email)
+    expect(instance.state.email).toEqual(email)
+    expect(instance.state.emailIsValid).toBeTruthy()
+
+    instance.setInput(passwordKey, password)
+    expect(instance.state.password).toEqual(password)
   })
 })
 
