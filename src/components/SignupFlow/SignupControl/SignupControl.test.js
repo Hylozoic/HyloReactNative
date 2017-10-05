90d33a21707d3f5243c0c864388f3550f32c58f4
Robbie Carlton <robbie.carlton@gmail.com>
2017-10-05 15:57:12 -0700

    SignupControl tests

--- src/components/SignupFlow/SignupControl/SignupControl.test.js
+++ src/components/SignupFlow/SignupControl/SignupControl.test.js
@@ -11,6 +11,7 @@ it('matches last snapshot', () => {
     value: 'Joe Bloggs',
     onChange: () => {},
     toggleSecureTextEntry: true,
+    toggleEditable: true,
     keyboardType: 'email-address',
     autoCapitalize: 'words',
     autoCorrect: true,
@@ -31,4 +32,86 @@ it('toggles the password', () => {
   expect(instance.state.securePassword).toEqual(true)
   instance.togglePassword()
   expect(instance.state.securePassword).toEqual(false)
+})
+
+it('sets editable to false if toggleEditable', () => {
+  var instance = ReactTestRenderer.create(<SignupControl />).root.instance
+  instance.componentDidMount()
+  expect(instance.state.editable).toEqual(true)
+
+  instance = ReactTestRenderer.create(<SignupControl toggleEditable />).root.instance
+  instance.componentDidMount()
+  expect(instance.state.editable).toEqual(false)
+})
+
+describe('on toggleEditable', () => {
+  it('toggles the state', () => {
+    const instance = ReactTestRenderer.create(<SignupControl toggleEditable />).root.instance
+    instance.setState({editable: true})
+    instance.toggleEditable()
+    expect(instance.state.editable).toEqual(false)
+    instance.toggleEditable()
+    expect(instance.state.editable).toEqual(true)
+  })
+})
+
+describe('on focus', () => {
+  it('calls input.focus ', () => {
+    const instance = ReactTestRenderer.create(<SignupControl />).root.instance
+    instance.input.focus = jest.fn()
+    instance.focus()
+    expect(instance.input.focus).toHaveBeenCalled()
+  })
+})
+
+describe('on blur', () => {
+  it('calls input.blur ', () => {
+    const instance = ReactTestRenderer.create(<SignupControl />).root.instance
+    instance.input.blur = jest.fn()
+    instance.blur()
+    expect(instance.input.blur).toHaveBeenCalled()
+  })
+})
+
+describe('on makeEditable', () => {
+  it('sets the state', () => {
+    const instance = ReactTestRenderer.create(<SignupControl toggleEditable />).root.instance
+    instance.setState({editable: false})
+    instance.makeEditable()
+    expect(instance.state.editable).toEqual(true)
+  })
+})
+
+describe('on isEditable', () => {
+  it('returns the state', () => {
+    const instance = ReactTestRenderer.create(<SignupControl toggleEditable />).root.instance
+    instance.setState({editable: false})
+    expect(instance.isEditable()).toEqual(false)
+    instance.setState({editable: true})
+    expect(instance.isEditable()).toEqual(true)
+  })
+})
+
+describe('on onSubmitEditing', () => {
+  it('does all the things', () => {
+    const props = {
+      toggleEditable: true,
+      onSubmitEditing: jest.fn()
+    }
+    const instance = ReactTestRenderer.create(<SignupControl {...props} />).root.instance
+    instance.setState({highlight: true})
+    instance.onSubmitEditing()
+    expect(instance.state.editable).toEqual(false)
+    expect(props.onSubmitEditing).toHaveBeenCalled()
+    expect(instance.state.highlight).toEqual(false)
+  })
+})
+
+describe('on highlightCheck', () => {
+  it('sets the state', () => {
+    const instance = ReactTestRenderer.create(<SignupControl />).root.instance
+    expect(instance.state.highlight).toEqual(false)
+    instance.highlightCheck()
+    expect(instance.state.highlight).toEqual(true)
+  })
 })
