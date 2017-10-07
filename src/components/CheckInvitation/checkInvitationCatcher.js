9e2988c8095944cc9f5fb8723abe9a881a87f872
Loren Johnson <loren@hellovenado.com>
2017-10-07 16:23:31 -0700

    Cleanup

--- src/components/CheckInvitation/checkInvitationCatcher.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import CheckInvitation from './index'
-// import { Linking } from 'react-native'
-
-export default function checkInvitationCatcher (Comp) {
-  return <CheckInvitation Comp={Comp} />
-}
