045ca8e437b7e369a12cd2fa7ee86a9a7aee3b3f
Andy Gimma <andy@hylo.com>
2017-10-12 17:10:51 -0500

    Show alert once. Add tests

--- src/components/SessionCheck/component.js
+++ src/components/SessionCheck/component.js
@@ -7,15 +7,19 @@ import { get } from 'lodash/fp'
 import { isIOS } from 'util/platform'
 
 export default class SessionCheck extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      allowShowAlert: false
+    }
+  }
+
   componentDidMount () {
     this.props.checkSession()
     this.props.checkVersion()
   }
 
   showAlert = (updateType) => {
-    this.setState({
-      alertActivated: true
-    })
     const storeName = isIOS ? 'App Store' : 'Play Store'
     const APP_STORE_LINK = 'https://itunes.apple.com/app/com.hylo.HyloA'
     const PLAY_STORE_LINK = 'https://play.google.com/store/apps/details?id=com.hylo.reactnative'
@@ -44,16 +48,25 @@ export default class SessionCheck extends React.Component {
     )
   }
 
+  componentWillReceiveProps = (nextProps) => {
+    const updateType = get('type', nextProps.showUpdateModal)
+    if (updateType) {
+      this.setState({
+        allowShowAlert: !this.state.allowShowAlert
+      })
+    }
+  }
+
   render () {
     const updateType = get('type', this.props.showUpdateModal)
-    const alertActivated = get('alertActivated', this.state)
+    const allowShowAlert = get('allowShowAlert', this.state)
     if (this.props.pending) {
       return <View style={mixins.allCentered}>
         <Text>Loading...</Text>
       </View>
     }
 
-    updateType && !alertActivated && this.showAlert(updateType)
+    updateType && allowShowAlert && this.showAlert(updateType)
 
     switch (this.props.loggedIn) {
       case true:
