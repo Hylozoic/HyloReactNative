99b06f044033ddd7407e09c9599425d76143657b
Robbie Carlton <robbie.carlton@gmail.com>
2017-10-12 11:01:53 -0700

    connector test

--- src/components/UserSettings/UserSettings.connector.js
+++ src/components/UserSettings/UserSettings.connector.js
@@ -1,11 +1,10 @@
-import { logout } from '../Login/actions'
+import { logout, loginWithFacebook } from '../Login/actions'
 import updateUserSettings from '../../store/actions/updateUserSettings'
 import { unlinkAccount } from './UserSettings.store'
 import { connect } from 'react-redux'
 import getMe from '../../store/selectors/getMe'
-import { loginWithFacebook } from '../Login/actions'
 
-function mapStateToProps (state, props) {
+export function mapStateToProps (state, props) {
   const cancel = () => props.navigation.goBack()
 
   return {
--- src/components/UserSettings/UserSettings.connector.test.js
+++ src/components/UserSettings/UserSettings.connector.test.js
@@ -1,5 +1,21 @@
-describe('', () => {
-  it('', () => {
+import { mapStateToProps, mapDispatchToProps } from './UserSettings.connector'
 
+describe('mapStateToProps', () => {
+  it('sets up cancel', () => {
+    const props = {
+      navigation: {
+        goBack: jest.fn()
+      }
+    }
+    const stateProps = mapStateToProps({}, props)
+    expect(stateProps).toMatchSnapshot()
+    stateProps.cancel()
+    expect(props.navigation.goBack).toHaveBeenCalled()
+  })
+})
+
+describe('mapDispatchToProps', () => {
+  it('has the right keys', () => {
+    expect(mapDispatchToProps).toMatchSnapshot()
   })
 })
