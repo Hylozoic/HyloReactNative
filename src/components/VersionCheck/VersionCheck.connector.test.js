6a2f1213d8fd2153b86edb1988f8ab8c64d9f0d3
Andy Gimma <andy@hylo.com>
2017-10-17 17:20:09 -0400

    Update tests

--- src/components/VersionCheck/VersionCheck.connector.test.js
+++ src/components/VersionCheck/VersionCheck.connector.test.js
@@ -1,12 +1,19 @@
 import { mapStateToProps, mapDispatchToProps } from './VersionCheck.connector.js'
 
-jest.mock('react-native-device-info')
+jest.mock('react-native-device-info', () => {
+  return {
+    default: {
+      getVersion: jest.fn()
+    },
+    getVersion: jest.fn(() => '2.0')
+  }
+})
 
 describe('mapStateToProps', () => {
   it('returns the right keys', () => {
     const state = {
       session: {
-        loggedIn: true
+        checkVersion: {type: true}
       },
       pending: {
         CHECK_VERSION: true
