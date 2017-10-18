aa5e8c5d339e0252da0ef4bb74746c024ab9c953
Andy Gimma <andy@hylo.com>
2017-10-18 16:10:48 -0400

    clean up tests

--- src/components/VersionCheck/VersionCheck.test.js
+++ src/components/VersionCheck/VersionCheck.test.js
@@ -18,55 +18,51 @@ describe('VersionCheck', () => {
     const actual = renderer.getRenderOutput()
     expect(actual).toMatchSnapshot()
   })
-  describe('VersionCheck', () => {
-    it('renders correctly with an updateType', () => {
-      const updateType = {
-        type: 'force',
-        title: 'Force title',
-        message: 'Force message'
-      }
-      const renderer = new ReactShallowRenderer()
-      renderer.render(<VersionCheck updateType={updateType} checkVersion={jest.fn()}><View /></VersionCheck>)
-      const actual = renderer.getRenderOutput()
-      expect(actual).toMatchSnapshot()
-    })
-    it('matches last snapshot with a suggested version update', () => {
-      const updateType = {
-        type: 'suggest'
-      }
-      const renderer = new ReactShallowRenderer()
-      renderer.render(<VersionCheck updateType={updateType} checkVersion={jest.fn()}><View /></VersionCheck>)
-      const actual = renderer.getRenderOutput()
-      expect(actual).toMatchSnapshot()
-    })
-    it('matches last snapshot with a forced version update', () => {
-      const updateType = {
-        type: 'force'
-      }
-      const renderer = new ReactShallowRenderer()
-      renderer.render(<VersionCheck updateType={updateType} checkVersion={jest.fn()}><View /></VersionCheck>)
-      const actual = renderer.getRenderOutput()
-      expect(actual).toMatchSnapshot()
-    })
-    it('matches last snapshot without a version update', () => {
-      const renderer = new ReactShallowRenderer()
-      renderer.render(<VersionCheck updateType={null} checkVersion={jest.fn()}><View /></VersionCheck>)
-      const actual = renderer.getRenderOutput()
-      expect(actual).toMatchSnapshot()
-    })
+  it('renders correctly with an updateType', () => {
+    const updateType = {
+      type: 'force',
+      title: 'Force title',
+      message: 'Force message'
+    }
+    const renderer = new ReactShallowRenderer()
+    renderer.render(<VersionCheck updateType={updateType} checkVersion={jest.fn()}><View /></VersionCheck>)
+    const actual = renderer.getRenderOutput()
+    expect(actual).toMatchSnapshot()
   })
-  describe('VersionCheck', () => {
-    it('renders correctly while pending', () => {
-      const updateType = {
-        type: 'force',
-        title: 'Force title',
-        message: 'Force message'
-      }
-      const renderer = new ReactShallowRenderer()
-      renderer.render(<VersionCheck updateType={updateType} checkVersion={jest.fn()} pending><View /></VersionCheck>)
-      const actual = renderer.getRenderOutput()
-      expect(actual).toMatchSnapshot()
-    })
+  it('matches last snapshot with a suggested version update', () => {
+    const updateType = {
+      type: 'suggest'
+    }
+    const renderer = new ReactShallowRenderer()
+    renderer.render(<VersionCheck updateType={updateType} checkVersion={jest.fn()}><View /></VersionCheck>)
+    const actual = renderer.getRenderOutput()
+    expect(actual).toMatchSnapshot()
+  })
+  it('matches last snapshot with a forced version update', () => {
+    const updateType = {
+      type: 'force'
+    }
+    const renderer = new ReactShallowRenderer()
+    renderer.render(<VersionCheck updateType={updateType} checkVersion={jest.fn()}><View /></VersionCheck>)
+    const actual = renderer.getRenderOutput()
+    expect(actual).toMatchSnapshot()
+  })
+  it('matches last snapshot without a version update', () => {
+    const renderer = new ReactShallowRenderer()
+    renderer.render(<VersionCheck updateType={null} checkVersion={jest.fn()}><View /></VersionCheck>)
+    const actual = renderer.getRenderOutput()
+    expect(actual).toMatchSnapshot()
+  })
+  it('renders correctly while pending', () => {
+    const updateType = {
+      type: 'force',
+      title: 'Force title',
+      message: 'Force message'
+    }
+    const renderer = new ReactShallowRenderer()
+    renderer.render(<VersionCheck updateType={updateType} checkVersion={jest.fn()} pending><View /></VersionCheck>)
+    const actual = renderer.getRenderOutput()
+    expect(actual).toMatchSnapshot()
   })
 })
 
