4fe4020c54cc7ae2f2ce386c4cc565cff4f3b6f2
Robbie Carlton <robbie.carlton@gmail.com>
2017-10-25 10:40:40 -0600

    snapshot

--- src/store/selectors/getMe.js
+++ src/store/selectors/getMe.js
@@ -1,6 +1,8 @@
 import { createSelector as ormCreateSelector } from 'redux-orm'
 import orm from '../../store/models'
 
+// TODO put this back to using selector once confirmed this isnt a memoization issue
+
 // const getMe = ormCreateSelector(
 //   orm,
 //   state => state.orm,
