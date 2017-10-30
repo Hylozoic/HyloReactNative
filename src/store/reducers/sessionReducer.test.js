bb33bd0682f2a87e303897cd433a19ace295bc51
Andy Gimma <andy@hylo.com>
2017-10-30 13:12:06 -0400

    Add check invitation test

--- src/store/reducers/sessionReducer.test.js
+++ src/store/reducers/sessionReducer.test.js
@@ -1,5 +1,6 @@
 import sessionReducer from './sessionReducer'
 import { CHECK_VERSION } from '../../components/VersionCheck/actions'
+import { CHECK_INVITATION } from '../../components/CheckInvitation/CheckInvitation.store'
 
 describe('on CHECK_VERSION', () => {
   it('returns the payload', () => {
@@ -15,5 +16,15 @@ describe('on CHECK_VERSION', () => {
       payload
     }
     expect(sessionReducer(state, action)).toEqual({checkVersion: payload})
+  })
+})
+
+describe('on CHECK_INVITATION', () => {
+  it('returns the payload', () => {
+    const state = {}
+    const action = {
+      type: CHECK_INVITATION
+    }
+    expect(sessionReducer(state, action)).toEqual({hasSignupLink: true})
   })
 })
