db4471f92caf67b3c85379383696bcce0c848333
Lawrence Wang <l@lw.io>
2017-10-30 19:25:39 -0700

    do the same thing in ThreadList we do in Thread

--- src/store/reducers/ormReducer/pushNotifications.js
+++ src/store/reducers/ormReducer/pushNotifications.js
@@ -5,16 +5,10 @@ export default function (session, action) {
   const { type, payload: notification } = action
   if (type !== RECEIVE_PUSH_NOTIFICATION) return
 
-  console.log(notification)
-
   const { path } = notification.payload.additionalData
   if (path.startsWith('/t/')) {
     showMessagesBadge(session)
-
-    // call reconnectFetchMessages in thread if it's open? or will socket reconnection
-    // take care of that?
-
-    // refresh thread list
   }
-  // TODO toggle notifications icon badge
+
+  // TODO toggle notifications list icon badge
 }
