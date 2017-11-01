b502db41822534d20095510686fec83bb9eb157a
Ray Marceau <razorman8669@users.noreply.github.com>
2017-11-01 12:48:54 -0400

    MBL-133 and MBL-132 user and moderator post deletion (#124)

---
+++ src/components/PostCard/PostHeader/PostHeader.store.js
@@ -0,0 +1,65 @@
+export const MODULE_NAME = 'PostHeader'
+
+// Constants
+export const DELETE_POST = `${MODULE_NAME}/DELETE_POST`
+export const DELETE_POST_PENDING = DELETE_POST + '_PENDING'
+export const REMOVE_POST = `${MODULE_NAME}/REMOVE_POST`
+export const REMOVE_POST_PENDING = REMOVE_POST + '_PENDING'
+
+// Action Creators
+export function deletePost (id) {
+  return {
+    type: DELETE_POST,
+    graphql: {
+      query: `mutation ($id: ID) {
+        deletePost(id: $id) {
+          success
+        }
+      }`,
+      variables: {
+        id
+      }
+    },
+    meta: {
+      optimistic: true,
+      id
+    }
+  }
+}
+
+export function removePost (postId, slug) {
+  return {
+    type: REMOVE_POST,
+    graphql: {
+      query: `mutation ($postId: ID, $slug: String) {
+        removePost(postId: $postId, slug: $slug) {
+          success
+        }
+      }`,
+      variables: {
+        postId,
+        slug
+      }
+    },
+    meta: {
+      optimistic: true,
+      postId,
+      slug
+    }
+  }
+}
+
+export function ormSessionReducer ({ Post }, { type, meta }) {
+  switch (type) {
+    case DELETE_POST_PENDING:
+      Post.withId(meta.id).delete()
+      break
+
+    case REMOVE_POST_PENDING:
+      let post = Post.withId(meta.postId)
+      const communities = post.communities.filter(c =>
+        c.slug !== meta.slug).toModelArray()
+      post.update({communities})
+      break
+  }
+}
--- src/store/reducers/ormReducer/sessionReducers.js
+++ src/store/reducers/ormReducer/sessionReducers.js
@@ -1,7 +1,9 @@
 export {
   ormSessionReducer as socketListenerReducer
 } from '../../../components/SocketListener/SocketListener.store'
-
+export {
+  ormSessionReducer as postHeaderReducer
+} from '../../../components/PostCard/PostHeader/PostHeader.store'
 export {
   default as pushNotificationReducer
 } from './pushNotifications'
