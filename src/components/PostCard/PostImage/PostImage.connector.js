ecd9b7029550f40fbd93849205b244f8eff3d550
Lawrence Wang <l@lw.io>
2017-11-07 14:16:11 -0800

    show thumbnails for additional images

--- src/components/PostCard/PostImage/PostImage.connector.js
+++ src/components/PostCard/PostImage/PostImage.connector.js
@@ -1,16 +1,17 @@
 import { connect } from 'react-redux'
 import getPost from '../../../store/selectors/getPost'
+import { get } from 'lodash/fp'
 
 export function mapStateToProps (state, props) {
   const post = getPost(state, {id: props.postId})
   if (!post) return {}
 
-  const image = post.attachments
-  .filter(a => a.type === 'image' && a.position === 0)
-  .first()
-  if (!image) return {}
+  const imageUrls = post.images()
+  .orderBy(get('position'))
+  .toRefArray()
+  .map(get('url'))
 
-  return {imageUrl: image.url}
+  return {imageUrls}
 }
 
 export default connect(mapStateToProps)
--- src/components/PostCard/PostImage/PostImage.js
+++ src/components/PostCard/PostImage/PostImage.js
@@ -1,7 +1,43 @@
-import SpaceFillingImage from '../../SpaceFillingImage'
 import React from 'react'
+import { isEmpty } from 'lodash'
+import { Dimensions, Image, ImageBackground } from 'react-native'
 
-export default function PostImage ({ imageUrl }) {
-  if (!imageUrl) return null
-  return <SpaceFillingImage imageUrl={imageUrl} style={{marginBottom: 12}} />
+export default function PostImage ({ imageUrls }) {
+  if (isEmpty(imageUrls)) return null
+  return <ImageBackground
+    style={styles.background}
+    imageStyle={styles.backgroundImage}
+    source={{uri: imageUrls[0]}}>
+    {imageUrls.length > 0 && imageUrls.slice(1).map(uri =>
+      <Image source={{uri}} key={uri + Math.random()} style={styles.thumbnail} />)}
+  </ImageBackground>
+}
+
+// this is from fudging and looking at how it turns out. since it's fixed, the
+// aspect ratio of the image is slightly different between the feed and post
+// details.
+const backgroundHeight = Dimensions.get('window').width * 0.62
+
+const styles = {
+  background: {
+    width: '100%',
+    height: backgroundHeight,
+    marginBottom: 12,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    padding: 8
+  },
+  backgroundImage: {
+    resizeMode: 'cover'
+  },
+  thumbnail: {
+    width: 48,
+    height: 48,
+    marginLeft: 8,
+    marginTop: 8,
+    borderWidth: 1,
+    borderColor: 'white'
+  }
 }
