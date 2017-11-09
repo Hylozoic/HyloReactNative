e470f44a986b4f22762e2cbb39a2b6f24912c998
Ray Marceau <razor2n4@gmail.com>
2017-11-09 14:01:05 -0800

    pull google services and plist for you

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -2,6 +2,20 @@ source ../.env
 
 if [ -z ${FACEBOOK_APP_ID_DEV+x} ]; then echo "ERROR: Missing appropriate config in .env. eg: FACEBOOK_APP_ID_DEV"; exit 1; else echo "Configuring Project:"; fi
 
+mytmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
+git clone -b android https://github.com/Hylozoic/code-signing.git $mytmpdir/android
+git clone -b ios https://github.com/Hylozoic/code-signing.git $mytmpdir/ios
+
+echo "Applying android/app/google-services.json"
+cp $mytmpdir/android/google-services.json ../android/app/
+echo "Applying android/app/debug.keystore"
+cp $mytmpdir/android/debug.keystore ../android/app/
+
+echo "Applying ios/debug.keystore"
+cp $mytmpdir/ios/GoogleService-Info.plist ../ios/
+
+rm -rf $mytmpdir
+
 echo "creating ios/debug.xcconfig"
 
 cat > ../ios/debug.xcconfig <<EOL
@@ -44,5 +58,9 @@ echo ""
 echo "IMPORTANT!!! Final Step:"
 echo "Add the following values to your ~/.gradle/gradle.properties":
 echo ""
+echo "HYLO_DEBUG_STORE_FILE=debug.keystore"
+echo "HYLO_DEBUG_KEY_ALIAS=androiddebugkey"
+echo "HYLO_DEBUG_STORE_PASSWORD=android"
+echo "HYLO_DEBUG_KEY_PASSWORD=android"
 echo "DEBUG_ONESIGNAL_APP_ID=$ONESIGNAL_APP_ID_DEV"
 echo "RELEASE_ONESIGNAL_APP_ID=$ONESIGNAL_APP_ID_RELEASE"
