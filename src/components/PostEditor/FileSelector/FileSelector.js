11b17fc255668ce0cccfbe199d21295bce7931aa
Lawrence Wang <l@lw.io>
2017-11-09 16:36:49 -0800

    add pending state to FileSelector

--- src/components/PostEditor/FileSelector/FileSelector.js
+++ src/components/PostEditor/FileSelector/FileSelector.js
@@ -6,46 +6,50 @@ import { DocumentPicker, DocumentPickerUtil } from 'react-native-document-picker
 import styles from './FileSelector.styles'
 import { cleanName } from '../../../store/models/Attachment'
 
-export default function FileSelector ({ upload, fileUrls, type, id, onAdd, onRemove }) {
-  const onPress = pick({
-    onChoice: onAdd,
-    upload,
-    type,
-    id
-  })
+export default class FileSelector extends React.PureComponent {
+  static defaultProps = {fileUrls: []}
+  state = {}
 
-  return <View style={styles.container}>
-    {fileUrls.map((url, index) => renderFileButton(url, index, onRemove))}
-    <TouchableOpacity onPress={onPress}
-      style={[styles.fileLabel, styles.addButton]}>
-      <Icon name='Paperclip' style={styles.addIcon} />
-      <Text style={styles.fileLabelText}>Add file</Text>
-    </TouchableOpacity>
-  </View>
-}
-FileSelector.defaultProps = {fileUrls: []}
+  pick = () => {
+    this.setState({pending: true})
+    const { upload, type, id, onError } = this.props
+    DocumentPicker.show({
+      filetype: [DocumentPickerUtil.allFiles()]
+    }, (err, result) => {
+      if (err) return alert(err.message)
 
-const pick = ({ upload, type, id, onError, onChoice }) => () => {
-  DocumentPicker.show({
-    filetype: [DocumentPickerUtil.allFiles()]
-  }, (err, result) => {
-    if (err) return alert(err.message)
-
-    const file = {
-      uri: result.uri,
-      name: result.fileName,
-      type: result.type
-    }
-
-    return upload(type, id, file)
-    .then(({ payload, error }) => {
-      if (error) {
-        onError && onError(payload.message)
-      } else {
-        onChoice({local: result.uri, remote: payload.url})
+      const file = {
+        uri: result.uri,
+        name: result.fileName,
+        type: result.type
       }
+
+      return upload(type, id, file)
+      .then(({ payload, error }) => {
+        this.setState({pending: false})
+        if (error) {
+          onError && onError(payload.message)
+        } else {
+          this.props.onAdd({local: result.uri, remote: payload.url})
+        }
+      })
     })
-  })
+  }
+
+  render () {
+    const { fileUrls, onRemove } = this.props
+    const { pending } = this.state
+    return <View style={styles.container}>
+      {fileUrls.map((url, index) => renderFileButton(url, index, onRemove))}
+      <TouchableOpacity onPress={this.pick}
+        style={[styles.fileLabel, styles.addButton]}>
+        <Icon name={pending ? 'Clock' : 'Paperclip'} style={styles.addIcon} />
+        <Text style={styles.fileLabelText}>
+          {pending ? 'Adding...' : 'Add file'}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  }
 }
 
 function renderFileButton (url, buttonIndex, onRemove) {
