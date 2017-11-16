812054677cc3879e56bca8df651b33d886f909fc
Lawrence Wang <l@lw.io>
2017-11-15 16:37:44 -0800

    fix timer / hot-reloading conflict

--- index.js
+++ index.js
@@ -4,21 +4,13 @@ import { AppRegistry, Platform } from 'react-native'
 import Timer from 'react-native-background-timer'
 
 if (Platform.OS === 'android') {
-  /*
-   * We get these long polling warnings in development, which can actually cause problems in
-   * production.  Here's a workaround (https://github.com/facebook/react-native/issues/12981),
-   * which ends up breaking `hot reloading` in development mode, hence the isProduction conditional here.
-   */
-  if (process.env.NODE_ENV === 'production') {
-    setTimeout = Timer.setTimeout.bind(Timer)
-    setInterval = Timer.setInterval.bind(Timer)
-    clearTimeout = Timer.clearTimeout.bind(Timer)
-    clearInterval = Timer.clearInterval.bind(Timer)
-  } else {
-    console.ignoredYellowBox = [
-      'Setting a timer'
-    ]
-  }
+  // We get these long polling warnings in development, which can actually cause
+  // problems in production.  Here's a workaround.
+  // https://github.com/facebook/react-native/issues/12981
+  setTimeout = (fn, ms = 0) => Timer.setTimeout(fn, ms)
+  setInterval = (fn, ms = 0) => Timer.setInterval(fn, ms)
+  clearTimeout = (fn, ms = 0) => Timer.clearTimeout(fn, ms)
+  clearInterval = (fn, ms = 0) => Timer.clearInterval(fn, ms)
 }
 
 AppRegistry.registerComponent('HyloReactNative', () => RootView)
