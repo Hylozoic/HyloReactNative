e71c9430ad6d9a00a904499439909e8ebe4a8c48
Rich Churcher <rich.churcher@gmail.com>
2017-11-22 10:02:43 +1300

    Remove redundant

--- src/components/Tabs/Home/Home.connector.js
+++ src/components/Tabs/Home/Home.connector.js
@@ -5,16 +5,12 @@ import getMe from '../../../store/selectors/getMe'
 import { mapWhenFocused } from 'util/connector'
 
 export function mapStateToProps (state, props) {
-  const { isFocused } = props
-  if (!isFocused) return props
-
   const currentUser = getMe(state, props)
   const communityId = state.currentCommunity ||
     (currentUser && get('id', currentUser.lastViewedCommunity()))
   return {
     communityId,
-    currentUser,
-    isFocused
+    currentUser
   }
 }
 
