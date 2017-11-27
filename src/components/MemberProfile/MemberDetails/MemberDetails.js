97ae4488eea2871fcfd4993e96abcf2bf455aeda
Robbie Carlton <robbie.carlton@gmail.com>
2017-11-27 16:23:21 -0700

    add back button handler for android

--- src/components/MemberProfile/MemberDetails/MemberDetails.js
+++ src/components/MemberProfile/MemberDetails/MemberDetails.js
@@ -3,7 +3,8 @@ import {
   Text,
   View,
   TouchableOpacity,
-  ScrollView
+  ScrollView,
+  BackHandler
 } from 'react-native'
 import Icon from '../../Icon'
 import { HeaderBackButton } from 'react-navigation'
@@ -45,6 +46,7 @@ export default class MemberDetails extends React.Component {
     this.props.navigation.setParams({
       goBack: this.goBack
     })
+    BackHandler.addEventListener('hardwareBackPress', this.goBack)
   }
 
   componentDidUpdate (prevProps) {
@@ -57,6 +59,10 @@ export default class MemberDetails extends React.Component {
         person: editableFields(this.props.person)
       })
     }
+  }
+
+  componentWillUnmount () {
+    BackHandler.removeEventListener('hardwareBackPress', this.goBack)
   }
 
   goBack = () => {
