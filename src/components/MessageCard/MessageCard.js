73c225ffe775588680cf591f54252676904ad463
Robbie Carlton <robbie.carlton@gmail.com>
2018-01-01 13:14:57 +0000

    links in messages

--- src/components/MessageCard/MessageCard.js
+++ src/components/MessageCard/MessageCard.js
@@ -1,19 +1,32 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import HTMLView from 'react-native-htmlview'
+import { StyleSheet, Text, View } from 'react-native'
+import { present, sanitize } from 'hylo-utils/text'
 import { any, bool, shape, string } from 'prop-types'
+import urlHandler from '../../util/urlHandler'
 
 import Avatar from '../Avatar'
 
 import styles from './MessageCard.style'
 
 export default function MessageCard ({ message }) {
-  const { createdAt, creator, suppressCreator, suppressDate, text } = message
+  const { createdAt, creator, suppressCreator, suppressDate, text, showTopic, showMember } = message
+
+  const presentedText = present(
+    sanitize(text).replace(/\n/g, '').replace('<p>&nbsp;</p>', ''))
+
+  const textStyles = [ styles.text, suppressCreator && styles.marginTopNoCreator ]
 
   return <View style={[ styles.container, suppressCreator && styles.padLeftNoAvatar ]}>
     {!suppressCreator && <Avatar avatarUrl={creator.avatarUrl} />}
     <View style={[ styles.body, suppressCreator && styles.padTopNoCreator ]}>
       {!suppressCreator && <Text style={styles.name}>{creator.name}</Text>}
-      <Text style={[ styles.text, suppressCreator && styles.marginTopNoCreator ]}>{text}</Text>
+      <HTMLView
+        onLinkPress={url => urlHandler(url, showMember, showTopic)}
+        addLineBreaks={false}
+        stylesheet={richTextStyles}
+        textComponentProps={{ style: textStyles }}
+        value={presentedText} />
       {!suppressDate && <Text style={styles.date}>{createdAt}</Text>}
     </View>
   </View>
@@ -33,3 +46,11 @@ MessageCard.propTypes = {
     text: string
   })
 }
+
+// TODO: remove duplication between here and PostBody
+
+const richTextStyles = StyleSheet.create({
+  a: {
+    color: '#0DC39F'
+  }
+})
--- src/util/urlHandler.js
+++ src/util/urlHandler.js
@@ -1,6 +1,8 @@
 import { Linking } from 'react-native'
 
 export default function urlHandler (url, showMember, showTopic, slug) {
+  // TODO This is insufficient. We need a generalized link handler that catches
+  // hylo links and redirects them internally
   const communityRoute = `/c/${slug}/`
   const variableRoute = url.substring(communityRoute.length)
   const [ prefix, suffix ] = variableRoute.split('/')
