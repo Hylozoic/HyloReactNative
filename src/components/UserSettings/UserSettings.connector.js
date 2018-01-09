8367933e3c1803bb47c4dc8373d4c4a0f47cba3d
Loren Johnson <loren@hellovenado.com>
2018-01-09 13:26:42 -0800

    Update tests

--- src/components/UserSettings/UserSettings.connector.js
+++ src/components/UserSettings/UserSettings.connector.js
@@ -1,13 +1,14 @@
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { get } from 'lodash/fp'
+import { resetToRoute, resetToMainRoute } from 'util/navigation'
 import { logout, loginWithFacebook } from '../Login/actions'
 import updateUserSettings from '../../store/actions/updateUserSettings'
 import { unlinkAccount } from './UserSettings.store'
-import { connect } from 'react-redux'
 import getMe from '../../store/selectors/getMe'
-import { bindActionCreators } from 'redux'
-import { resetToRoute, resetToMainRoute } from 'util/navigation'
 
 export function mapStateToProps (state, props) {
-  const resettingPassword = props.navigation.state.routeName === 'PasswordReset'
+  const resettingPassword = get('navigation.state.routeName', props) === 'PasswordReset'
   const cancel = () => resettingPassword
     ? resetToMainRoute(props.navigation)
     : props.navigation.goBack()
