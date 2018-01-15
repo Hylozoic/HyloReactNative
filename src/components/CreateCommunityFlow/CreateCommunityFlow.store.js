3ec26164118fe10a84e4d9bb28977cda1b0e8a3f
Andy Gimma <andy@hylo.com>
2018-01-15 13:53:28 -0500

    Update tests remove console statements

--- src/components/CreateCommunityFlow/CreateCommunityFlow.store.js
+++ src/components/CreateCommunityFlow/CreateCommunityFlow.store.js
@@ -2,7 +2,6 @@ export const MODULE_NAME = 'CreateCommunityName'
 export const SAVE_COMMUNITY_NAME = `${MODULE_NAME}/SAVE_COMMUNITY_NAME`
 
 export default function reducer (state = {}, action) {
-  console.log('reducer', state, action)
   const { type, payload } = action
   switch (type) {
     case SAVE_COMMUNITY_NAME:
@@ -15,7 +14,6 @@ export default function reducer (state = {}, action) {
 }
 
 export function saveCommunityName (name) {
-  console.log('saveCommunityName name', name)
   return {
     type: SAVE_COMMUNITY_NAME,
     payload: name
