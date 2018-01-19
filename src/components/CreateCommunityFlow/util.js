c36f3229add581cc8af72d1f1261c6ebde6bf660
Andy Gimma <andy@hylo.com>
2018-01-18 20:29:04 -0500

    Remove console statements

--- src/components/CreateCommunityFlow/util.js
+++ src/components/CreateCommunityFlow/util.js
@@ -2,7 +2,6 @@ export const slugValidatorRegex = /^[0-9a-z-]{2,40}$/
 export const invalidSlugMessage = 'URLs must have between 2 and 40 characters, and can only have lower case letters, numbers, and dashes.'
 
 export function formatDomainWithUrl (communityDomain) {
-  console.log('formatDomainWithUrl communityDomain', communityDomain)
   if (!communityDomain) return
   let formattedDomain = communityDomain.replace(/hylo\.com\/c\/?/, '')
   if (formattedDomain !== '') {
@@ -12,7 +11,6 @@ export function formatDomainWithUrl (communityDomain) {
 }
 
 export function removeUrlFromDomain (communityDomain) {
-  console.log('communityDomain', communityDomain)
   if (!communityDomain) return
   return communityDomain.replace('hylo.com/c/', '')
 }
