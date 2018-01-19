5e46728308f80f784cf9bcc637b0c9ad7730cb3e
Robbie Carlton <robbie.carlton@gmail.com>
2018-01-19 11:22:01 -0700

    tests

--- src/components/Tabs/Topics/Topics.test.js
+++ src/components/Tabs/Topics/Topics.test.js
@@ -1,11 +1,40 @@
 import 'react-native'
 import React from 'react'
 import ReactShallowRenderer from 'react-test-renderer/shallow'
-import Topics from './index'
+import Topics from './Topics'
 
-it('renders correctly with all=true, and no community or user', () => {
+jest.mock('react-native-device-info')
+
+it('renders correctly a community and topics', () => {
   const renderer = new ReactShallowRenderer()
-  renderer.render(<Topics />)
+  const props = {
+    community: {
+      name: 'Foomunity',
+      bannerUrl: 'community-banner.png'
+    },
+    topics: [1, 2, 3],
+    toggleTopicSubscribe: () => {},
+    goToTopic: () => {}
+  }
+  renderer.render(<Topics {...props} />)
+  const actual = renderer.getRenderOutput()
+
+  expect(actual).toMatchSnapshot()
+})
+
+it('renders correctly with pending=true', () => {
+  const renderer = new ReactShallowRenderer()
+  const props = {
+    community: {
+      name: 'Foomunity',
+      bannerUrl: 'community-banner.png'
+    },
+    topics: [1, 2, 3],
+    pending: true,
+    toggleTopicSubscribe: () => {},
+    goToTopic: () => {}
+  }
+  renderer.render(<Topics {...props} />)
   const actual = renderer.getRenderOutput()
 
   expect(actual).toMatchSnapshot()
