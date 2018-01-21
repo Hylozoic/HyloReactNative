94e4f801a9cd42452b6766e0781b6989b4ad21b1
Andy Gimma <andy@hylo.com>
2018-01-20 20:14:51 -0500

    Add error state to snapshot test

--- src/components/CreateCommunityFlow/CreateCommunityUrl/CreateCommunityUrl.test.js
+++ src/components/CreateCommunityFlow/CreateCommunityUrl/CreateCommunityUrl.test.js
@@ -1,6 +1,7 @@
 import 'react-native'
 import React from 'react'
 import ReactShallowRenderer from 'react-test-renderer/shallow'
+import ReactTestRenderer from 'react-test-renderer'
 import CreateCommunityUrl from './CreateCommunityUrl'
 
 jest.mock('../../KeyboardFriendlyView', () => 'KeyboardFriendlyView')
@@ -8,10 +9,15 @@ jest.mock('react-native-device-info')
 
 it('matches last snapshot', () => {
   const renderer = new ReactShallowRenderer()
-  const props = {}
 
-  renderer.render(<CreateCommunityUrl {...props} />)
+  renderer.render(<CreateCommunityUrl />)
   const actual = renderer.getRenderOutput()
-
   expect(actual).toMatchSnapshot()
+})
+
+it('matches last snapshot when visible is true', () => {
+  const renderer = ReactTestRenderer.create(<CreateCommunityUrl />)
+  const instance = renderer.getInstance()
+  instance.setState({error: 'This is an error'})
+  expect(renderer.toJSON()).toMatchSnapshot()
 })
