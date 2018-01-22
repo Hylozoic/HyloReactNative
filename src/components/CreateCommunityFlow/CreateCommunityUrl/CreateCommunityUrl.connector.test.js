0ca22a904d9e9d6fa9444bb648ef66d133c1a0c1
Andy Gimma <andy@hylo.com>
2018-01-21 19:51:41 -0500

    CreateCommunityUrl connector test

---
+++ src/components/CreateCommunityFlow/CreateCommunityUrl/CreateCommunityUrl.connector.test.js
@@ -0,0 +1,49 @@
+import {
+  mapStateToProps,
+  mapDispatchToProps,
+  mergeProps
+} from './CreateCommunityUrl.connector'
+import { FETCH_URL_EXISTS } from '../CreateCommunityFlow.store'
+
+const communityUrl = 'my-url'
+
+const state = {
+  CreateCommunityFlow: {
+    communityUrl,
+    urlExists: false
+  },
+  pending: {
+    [FETCH_URL_EXISTS]: true
+  }
+}
+
+describe('mapStateToProps', () => {
+  it('returns the right keys', () => {
+    expect(mapStateToProps(state, {})).toMatchSnapshot()
+  })
+})
+
+describe('mapDispatchToProps', () => {
+  it('maps the action generators', () => {
+    expect(mapDispatchToProps.hasOwnProperty('fetchCommunityExists')).toBeTruthy()
+    expect(mapDispatchToProps.hasOwnProperty('saveCommunityUrl')).toBeTruthy()
+    expect(mapDispatchToProps.saveCommunityUrl(communityUrl)).toMatchSnapshot()
+  })
+})
+
+describe('mergeProps', () => {
+  it('merges the props', () => {
+    const dispatchProps = {
+      saveCommunityName: jest.fn(name => Promise.resolve({}))
+    }
+    const ownProps = {
+      navigation: {
+        navigate: jest.fn()
+      }
+    }
+    const mergedProps = mergeProps(state, dispatchProps, ownProps)
+    expect(mergedProps).toMatchSnapshot()
+    mergedProps.goToCreateCommunityReview()
+    expect(ownProps.navigation.navigate).toHaveBeenCalled()
+  })
+})
