356d76df365c921dd64e06276c1f7fa33bfb2b5b
Andy Gimma <andy@hylo.com>
2018-01-21 19:39:21 -0500

    CreateCommunityName connector tests

---
+++ src/components/CreateCommunityFlow/CreateCommunityName/CreateCommunityName.connector.test.js
@@ -0,0 +1,43 @@
+import {
+  mapStateToProps,
+  mapDispatchToProps,
+  mergeProps
+} from './CreateCommunityName.connector'
+
+const communityName = 'name'
+
+const state = {
+  CreateCommunityFlow: {
+    communityName
+  }
+}
+
+describe('mapStateToProps', () => {
+  it('returns the right keys', () => {
+    expect(mapStateToProps(state, {})).toMatchSnapshot()
+  })
+})
+
+describe('mapDispatchToProps', () => {
+  it('maps the action generators', () => {
+    expect(mapDispatchToProps.hasOwnProperty('saveCommunityName')).toBeTruthy()
+    expect(mapDispatchToProps.saveCommunityName(communityName)).toMatchSnapshot()
+  })
+})
+
+describe('mergeProps', () => {
+  it('merges the props', () => {
+    const dispatchProps = {
+      saveCommunityName: jest.fn(name => Promise.resolve({}))
+    }
+    const ownProps = {
+      navigation: {
+        navigate: jest.fn()
+      }
+    }
+    const mergedProps = mergeProps(state, dispatchProps, ownProps)
+    expect(mergedProps).toMatchSnapshot()
+    mergedProps.goToCreateCommunityUrl()
+    expect(ownProps.navigation.navigate).toHaveBeenCalled()
+  })
+})
