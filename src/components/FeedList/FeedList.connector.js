c0d54f07ef5150026c0060fe7a3753462b5f861b
Loren Johnson <loren@hellovenado.com>
2018-04-02 16:27:18 -0700

    Remove console log

--- src/components/FeedList/FeedList.connector.js
+++ src/components/FeedList/FeedList.connector.js
@@ -53,7 +53,6 @@ export function mapStateToProps (state, props) {
   })
   const pending = state.pending[FETCH_POSTS]
 
-  console.log('p', getPosts(state, queryProps).map(p => presentPost(p, get('id', community))))
   return {
     posts: getPosts(state, queryProps).map(p => presentPost(p, get('id', community))),
     sortBy,
