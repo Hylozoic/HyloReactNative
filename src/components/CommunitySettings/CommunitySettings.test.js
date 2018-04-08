a49d8c068b6bc88c6e07ee35c31dea1d462e1ac2
Ray Marceau <razor2n4@gmail.com>
2018-04-07 18:27:14 -0700

    tests navigation

--- src/components/CommunitySettings/CommunitySettings.test.js
+++ src/components/CommunitySettings/CommunitySettings.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactShallowRenderer from 'react-test-renderer/shallow'
 import CommunitySettings from './CommunitySettings'
+import ReactTestRenderer from 'react-test-renderer'
 
 describe('CommunitySettings', () => {
   it('matches the last snapshot', () => {
@@ -14,5 +15,14 @@ describe('CommunitySettings', () => {
   it('has navigation options', () => {
     const props = {navigation: {state: {params: {}}}}
     expect(CommunitySettings.navigationOptions(props)).toMatchSnapshot()
+  })
+
+  it('navigates', () => {
+    const props = {navigation: {navigate: jest.fn()}}
+
+    const instance = ReactTestRenderer.create(<CommunitySettings {...props} />).getInstance()
+
+    instance.navigate('somePath')
+    expect(props.navigation.navigate).toHaveBeenCalledWith('somePath')
   })
 })
