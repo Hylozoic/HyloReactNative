4126cd5f3878e455a502d27939d0a37893a9b81f
Loren Johnson <loren@hellovenado.com>
2018-04-10 20:57:21 -0700

    MBL-484 Changes related to bitrise builds

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -6,20 +6,24 @@ cd ../
 
 echo "Root Directory $PWD"
 
-source .env
+export $(cat .env | grep -v ^# | xargs)
 
 if [ -z ${FACEBOOK_APP_ID_DEBUG+x} ]; then echo "ERROR: Missing appropriate config in .env. eg: FACEBOOK_APP_ID_DEBUG"; exit 1; else echo "Configuring Project:"; fi
 
 mytmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
-git clone -b android https://github.com/Hylozoic/code-signing.git $mytmpdir/android
-git clone -b ios https://github.com/Hylozoic/code-signing.git $mytmpdir/ios
+git clone -b android git@github.com:Hylozoic/code-signing.git $mytmpdir/android
+git clone -b ios git@github.com:Hylozoic/code-signing.git $mytmpdir/ios
 
 echo "Applying android/app/google-services.json"
 cp $mytmpdir/android/google-services.json android/app/
 echo "Applying android/app/debug.keystore"
 cp $mytmpdir/android/debug.keystore android/app/
+echo "Applying android/app/hylo-release-original.keystore"
+cp $mytmpdir/android/hylo-release-original.keystore android/app/
+echo "Applying android/app/hylo-release-key-2017-08.keystore"
+cp $mytmpdir/android/hylo-release-key-2017-08.keystore android/app/
 
-echo "Applying ios/debug.keystore"
+echo "Applying ios/GoogleService-Info.plist"
 cp $mytmpdir/ios/GoogleService-Info.plist ios/
 
 trap 'rm -rf "$mytmpdir"' EXIT
@@ -62,13 +66,26 @@ cat > android/app/src/release/res/values/strings.xml <<EOL
 
 EOL
 
-echo ""
-echo "IMPORTANT!!! Final Step:"
-echo "Add the following values to your ~/.gradle/gradle.properties":
-echo ""
-echo "HYLO_DEBUG_STORE_FILE=debug.keystore"
-echo "HYLO_DEBUG_KEY_ALIAS=androiddebugkey"
-echo "HYLO_DEBUG_STORE_PASSWORD=android"
-echo "HYLO_DEBUG_KEY_PASSWORD=android"
-echo "DEBUG_ONESIGNAL_APP_ID=$ONESIGNAL_APP_ID_DEBUG"
-echo "RELEASE_ONESIGNAL_APP_ID=$ONESIGNAL_APP_ID_RELEASE"
+read -r -d "" gradleProperties <<EOF
+HYLO_DEBUG_STORE_FILE=debug.keystore
+HYLO_DEBUG_KEY_ALIAS=androiddebugkey
+HYLO_DEBUG_STORE_PASSWORD=android
+HYLO_DEBUG_KEY_PASSWORD=android
+DEBUG_ONESIGNAL_APP_ID=$ONESIGNAL_APP_ID_DEBUG
+RELEASE_ONESIGNAL_APP_ID=$ONESIGNAL_APP_ID_RELEASE
+HYLO_RELEASE_STORE_FILE=hylo-release-original.keystore
+HYLO_RELEASE_KEY_ALIAS=MyAndroidKey
+HYLO_RELEASE_STORE_PASSWORD=$HYLO_RELEASE_STORE_PASSWORD
+HYLO_RELEASE_KEY_PASSWORD=$HYLO_RELEASE_KEY_PASSWORD
+EOF
+
+if [ -z ${CI+x} ]; then
+    echo "IMPORTANT!!! Final Step:"
+    echo "Add the following values to your ~/.gradle/gradle.properties":
+    echo ""
+    echo "$gradleProperties"
+else
+    echo "Creating ~/.gradle/gradle.properties for BitRise (CI) environment"
+    mkdir -p $HOME/.gradle
+    echo "$gradleProperties" > "$HOME/.gradle/gradle.properties"
+fi
