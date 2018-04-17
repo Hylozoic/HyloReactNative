c25f7f873b955085c6b7b53b6949d4c382a83a8b
Ray Marceau <razor2n4@gmail.com>
2018-04-17 14:13:51 -0700

    add hitslop to settings button

--- src/components/DrawerMenu/DrawerMenu.js
+++ src/components/DrawerMenu/DrawerMenu.js
@@ -48,7 +48,7 @@ export default class DrawerMenu extends Component {
       <View style={styles.communityHeader}>
         <Image source={{uri: currentCommunity.avatarUrl}} style={styles.communityHeaderAvatar} />
         <Text style={styles.communityHeaderText}>{currentCommunity.name}</Text>
-        {canModerateCurrentCommunity && <TouchableOpacity onPress={goToCommunitySettingsMenu} style={styles.communityHeaderSettingsButton}>
+        {canModerateCurrentCommunity && <TouchableOpacity onPress={goToCommunitySettingsMenu} hitSlop={{top: 5, bottom: 5, left: 10, right: 10}} style={styles.communityHeaderSettingsButton}>
           <Icon style={styles.communityHeaderSettingsButtonIcon} name='Settings' />
           <Text style={styles.communityHeaderSettingsButtonText}>Settings</Text>
         </TouchableOpacity>}
