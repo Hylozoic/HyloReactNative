e6436413ebd35709b6109a4bde6bc741d54622de
Rich Churcher <rich.churcher@gmail.com>
2018-05-05 01:43:49 +1200

    Styles, remove unused functions, wire up add

--- src/components/ModeratorSettings/ModeratorSettings.js
+++ src/components/ModeratorSettings/ModeratorSettings.js
@@ -4,6 +4,8 @@ import { get, isEmpty } from 'lodash/fp'
 
 import header from 'util/header'
 import Avatar from '../Avatar'
+import Icon from '../Icon'
+import LoadingScreen from '../Loading'
 import { SearchType } from '../Editor/Search/Search.store'
 import Search from '../Editor/Search'
 
@@ -29,10 +31,6 @@ export default class ModeratorSettings extends Component {
     }
   }
 
-  componentWillUnmount () {
-    this.props.clearModeratorSuggestions()
-  }
-
   removeModerator = (id) => {
     Alert.alert(
       'Remove Moderator',
@@ -46,40 +44,19 @@ export default class ModeratorSettings extends Component {
     )
   }
 
-  addModerator = mod => {
-    console.log('Add this mod?', mod)
-
-    // this.props.addModerator(moderatorToAdd)
-    // this.clearAutocomplete()
+  addModerator = ({ id }) => {
+    this.props.addModerator(id)
+    this.cancelPersonPicker()
   }
 
   cancelPersonPicker = () => this.setState({ showPicker: false })
 
-  _renderSeparator = () => {
-    return (
-      <View
-        style={{
-          height: 1,
-          width: '90%',
-          marginLeft: '5%',
-          backgroundColor: '#CED0CE'
-        }}
-      />
-    )
-  }
-
   isMe = (id) => this.props.currentUser.id === id
 
-  _renderModeratorRow = ({item}) => (
-    <ModeratorRow moderator={item} showMember={this.props.showMember} removeModerator={this.isMe(item.id) ? null : this.removeModerator} />
-  )
-
-  _renderAutocompleteItem = ({ id, name, avatarUrl }) => (
-    <TouchableOpacity style={styles.autocompleteItem} onPress={() => this.selectModeratorToAdd(id, name)}>
-      <Avatar style={{width: 50}} avatarUrl={avatarUrl} />
-      <Text style={{flex: 1}}>{name}</Text>
-    </TouchableOpacity>
-  )
+  _renderModeratorRow = ({item}) => <ModeratorRow
+    moderator={item}
+    showMember={this.props.showMember}
+    removeModerator={this.isMe(item.id) ? null : this.removeModerator} />
 
   showPersonPicker = () => this.setState({ showPicker: true })
 
@@ -88,7 +65,7 @@ export default class ModeratorSettings extends Component {
     const { showPicker } = this.state
 
     if (isEmpty(moderators)) {
-      return <Text>Loading...</Text>
+      return <LoadingScreen />
     }
 
     if (showPicker) {
@@ -103,14 +80,13 @@ export default class ModeratorSettings extends Component {
       style={styles.container}
       data={moderators}
       keyboardShouldPersistTaps='always'
-      ItemSeparatorComponent={this._renderSeparator}
       keyExtractor={item => item.id.toString()}
       renderItem={this._renderModeratorRow}
-      ListHeaderComponent={<View style={styles.headerContainer}><Text style={styles.headerText}>{community.name}</Text></View>}
-      ListFooterComponent={<View style={styles.addModeratorContainer}>
+      ListHeaderComponent={<View style={styles.headerContainer}>
+        <Text style={styles.headerText}>{community.name}</Text>
         <View style={styles.addNewContainer}>
           <TouchableOpacity onPress={this.showPersonPicker}>
-            <Text style={styles.addNewButton}>+ Add New</Text>
+            <Text style={styles.addNewButton}><Icon name='Plus' green /> Add New</Text>
           </TouchableOpacity>
         </View>
       </View>} />
@@ -120,7 +96,7 @@ export default class ModeratorSettings extends Component {
 export function ModeratorRow ({moderator, showMember, removeModerator}) {
   return <TouchableOpacity style={styles.row} onPress={() => showMember(moderator.id)}>
     {moderator.avatarUrl && <Avatar style={{width: 50}} avatarUrl={moderator.avatarUrl} />}
-    <Text style={{flex: 1}}>{moderator.name}</Text>
+    <Text style={styles.moderatorName}>{moderator.name}</Text>
     {removeModerator && <TouchableOpacity hitSlop={{top: 15, bottom: 15, left: 10, right: 10}} onPress={() => removeModerator(moderator.id)}>
       <Text style={styles.removeButton}>Remove</Text>
     </TouchableOpacity>}
