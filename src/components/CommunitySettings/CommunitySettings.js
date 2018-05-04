a3e0f6994333d427c8be36655f9256d98157cd24
Andy Gimma <andy@hylo.com>
2018-05-04 17:30:45 -0400

    ScrollView changes for Android

--- src/components/CommunitySettings/CommunitySettings.js
+++ src/components/CommunitySettings/CommunitySettings.js
@@ -132,8 +132,8 @@ export default class CommunitySettings extends React.Component {
 
     const { name, description, location } = this.state.edits
 
-    return <KeyboardFriendlyView style={styles.keyboardFriendlyContainer}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+    return <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+      <KeyboardFriendlyView style={styles.keyboardFriendlyContainer}>
         <TextInput
           style={styles.nameInput}
           onChangeText={this.updateField('name')}
@@ -159,8 +159,8 @@ export default class CommunitySettings extends React.Component {
           value={location}
           onChangeText={this.updateField('location')}
           underlineColorAndroid='transparent' />
+        </KeyboardFriendlyView>
       </ScrollView>
-    </KeyboardFriendlyView>
   }
 }
 
