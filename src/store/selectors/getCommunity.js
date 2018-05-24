0bea32bded799a97fe0e5f6e07bcf6ea6a0a96ed
Ray Marceau <razor2n4@gmail.com>
2018-05-23 17:31:47 -0700

    don't use cached selectors for getCommunity/getNetwork since the ormCreateSelector doesn't work well with it.

--- src/store/selectors/getCommunity.js
+++ src/store/selectors/getCommunity.js
@@ -1,23 +1,15 @@
 import orm from '../models'
 import { createSelector as ormCreateSelector } from 'redux-orm'
 import { get } from 'lodash/fp'
-import createCachedSelector from 're-reselect'
-
-const getSession = ormCreateSelector(
-  orm,
-  state => state.orm,
-  session => session
-)
 
 /**
  * gets community from slug OR id
  */
-const getCommunity = createCachedSelector(
-  getSession,
+const getCommunity = ormCreateSelector(
+  orm,
+  state => state.orm,
   (state, props) => get('id', props),
   (state, props) => get('slug', props),
   ({ Community }, id, slug) => Community.safeGet({id, slug})
-)(
-  (state, props) => `${props.id}:${props.slug}`
 )
 export default getCommunity
--- src/store/selectors/getNetwork.js
+++ src/store/selectors/getNetwork.js
@@ -1,23 +1,15 @@
 import orm from '../models'
 import { createSelector as ormCreateSelector } from 'redux-orm'
 import { get } from 'lodash/fp'
-import createCachedSelector from 're-reselect'
-
-const getSession = ormCreateSelector(
-  orm,
-  state => state.orm,
-  session => session
-)
 
 /**
  * gets network from slug OR id
  */
-const getNetwork = createCachedSelector(
-  getSession,
+const getNetwork = ormCreateSelector(
+  orm,
+  state => state.orm,
   (state, props) => get('id', props),
   (state, props) => get('slug', props),
   ({ Network }, id, slug) => Network.safeGet({id, slug})
-)(
-  (state, props) => props ? `${props.id}:${props.slug}` : 0
 )
 export default getNetwork
