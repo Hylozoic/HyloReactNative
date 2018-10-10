4c5d538049bc6025aadd70827a81a6733e911b73
Loren Johnson <loren@hellovenado.com>
2018-10-09 18:05:16 -0700

    Add ProjectMembers test

---
+++ src/components/ProjectMembers/ProjectMembers.test.js
@@ -0,0 +1,26 @@
+import 'react-native'
+import React from 'react'
+import ReactShallowRenderer from 'react-test-renderer/shallow'
+import ProjectMembers from './ProjectMembers'
+
+it('renders as expected', () => {
+  const members = [{id: 'member1'}, {id: 'member2'}]
+  const props = {
+    navigation: {
+      state: {
+        params: {
+          members
+        }
+      },
+      navigate: () => {}
+    }
+  }
+
+  const renderer = new ReactShallowRenderer()
+  renderer.render(
+    <ProjectMembers {...props} />
+  )
+  const actual = renderer.getRenderOutput()
+
+  expect(actual).toMatchSnapshot()
+})
