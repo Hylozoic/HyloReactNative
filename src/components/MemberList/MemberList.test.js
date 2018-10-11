c6dc307ebec583d902dadd1003749f805fde1de7
Loren Johnson <loren@hellovenado.com>
2018-10-11 14:13:18 -0700

    temporary commit to change computers

--- src/components/MemberList/MemberList.test.js
+++ src/components/MemberList/MemberList.test.js
@@ -4,6 +4,7 @@ import ReactShallowRenderer from 'react-test-renderer/shallow'
 import MemberList from './MemberList'
 
 describe('MemberList', () => {
+
   it('renders with default props with default non-server search', () => {
     const renderer = new ReactShallowRenderer()
     const testProps = {
@@ -23,6 +24,25 @@ describe('MemberList', () => {
   })
 
   it('renders with default props and server search', () => {
+    const renderer = new ReactShallowRenderer()
+    const testProps = {
+      isServerSearch: true,
+      hasMore: true,
+      fetchMembers: jest.fn(),
+      members: [
+        {id: '1', name: 'Loren'},
+        {id: '2', name: 'Robbie'}
+      ]
+    }
+
+    renderer.render(
+      <MemberList {...testProps} />
+    )
+
+    expect(testProps.fetchMembers).toHaveBeenCalled()
+  })
+
+  it('runs call for new results if server search', () => {
     const renderer = new ReactShallowRenderer()
     const testProps = {
       isServerSearch: true,
