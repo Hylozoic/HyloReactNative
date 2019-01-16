cc333e5b93a6bcc8da9f228024328569b2ece725
Loren Johnson <loren@hellovenado.com>
2019-01-15 16:31:37 -0800

    Update scopedGetRecentContacts.js

--- src/store/selectors/scopedGetRecentContacts.js
+++ src/store/selectors/scopedGetRecentContacts.js
@@ -7,9 +7,7 @@ import { FETCH_RECENT_CONTACTS } from 'store/constants'
 export default function scopedGetRecentContacts (_, { scope }) {
   if (!scope) throw new Error('`scope` prop is required for a querySearchTerm selection')
 
-  const queryResultsScope = scope
-    ? `${scope}/${FETCH_RECENT_CONTACTS}`
-    : FETCH_RECENT_CONTACTS
+  const queryResultsScope = `${scope}/${FETCH_RECENT_CONTACTS}`
   const getRecentContactsResults = makeGetQueryResults(queryResultsScope)
 
   return makeQueryResultsModelSelector(
