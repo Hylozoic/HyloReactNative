5b3290d401e4a2b4fc145357d2d9725a2a49c2b8
Loren Johnson <loren@hellovenado.com>
2020-05-24 11:03:42 -1000

    Debug building and running in iOS (minus env vars config)

--- scripts/clean-rebuild.sh
+++ scripts/clean-rebuild.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# A somewhat out of date refernce point:
+# 
+#  https://gist.github.com/jarretmoses/c2e4786fd342b3444f3bc6beff32098d
+
 # Force script to be called from root (which is 1 level up from the directory with this script)
 cd "$(dirname "$0")"
 cd ../
@@ -11,13 +15,10 @@ watchman watch-del-all
 
 echo "Remove node modules: 'rm -rf node_modules'"
 rm -rf node_modules
-echo "Remove tmp/react: 'rm -rf $TMPDIR/react-*'"
-rm -rf $TMPDIR/react-*
-echo "Remove tmp/npm: 'rm -rf $TMPDIR/npm-*'"
-rm -rf $TMPDIR/npm-*
 
 echo "'yarn cache clean'"
 yarn cache clean
+
 echo "'yarn install'"
 yarn install
 
@@ -33,9 +34,22 @@ else
   cd ../
 fi
 
+# Probably no longer relevant
+echo "Remove tmp/react: 'rm -rf $TMPDIR/react-*'"
+rm -rf $TMPDIR/react-*
+
+echo "Remove tmp/npm: 'rm -rf $TMPDIR/npm-*'"
+rm -rf $TMPDIR/npm-*
+
+echo "Remove metro bundler and haste caches"
+rm -rf $TMPDIR/metro-*
+rm -rf $TMPDIR/haste-map-*
+
 echo "'./android/gradlew clean -p ./android/'"
 ./android/gradlew clean -p ./android/
+
 echo "'rm -rf ios/build'"
 rm -rf ios/build
+
 echo "'yarn start --reset-cache'"
 yarn start --reset-cache
