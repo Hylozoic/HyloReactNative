1dc4324f4096b5c15bbc1013f87bd2be7deb7bd7
Loren Johnson <loren@hellovenado.com>
2020-05-24 19:38:43 -1000

    iOS run broken, upgrading anyway

--- android/app/src/main/java/com/hyloreactnative/MainApplication.java
+++ android/app/src/main/java/com/hyloreactnative/MainApplication.java
@@ -14,7 +14,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 
-
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
