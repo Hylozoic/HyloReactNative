c9cc2386fbacd82b60f4a2f8d5adaf9ca1731c6e
Loren Johnson <lorenjohnson@gmail.com>
2020-06-08 19:01:28 -1000

    Getting OneSignal back on the track

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-global-assign */
-import React from 'react'
+import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import RootView from './src/components/RootView'
 import { AppRegistry, Platform } from 'react-native'
 import Timer from 'react-native-background-timer'
 import * as Sentry from '@sentry/react-native'
+import OneSignal from 'react-native-onesignal'
 import ErrorBoundary from './src/components/ErrorBoundary'
 import { isDev } from './src/config'
 import getStore from './src/store'
@@ -34,12 +35,57 @@ AppRegistry.registerComponent(appName, () => AppContainer)
 //   whyDidYouUpdate(React, { exclude: [/^Connect/, /^Icon/, /^TabBarIcon/, /^LinearGradient/] })
 // }
 
-function AppContainer () {
-  return <ErrorBoundary>
-    <Provider store={store}>
-      <RootView />
-    </Provider>
-  </ErrorBoundary>
-}
+// // For OneSignal
+// function myiOSPromptCallback(permission){
+//   // do something with permission value
+// }
 
-export default AppContainer
+export default class AppContainer extends Component {
+  constructor(properties) {
+    super(properties);
+    // Remove this method to stop OneSignal Debugging 
+    OneSignal.setLogLevel(6, 0);
+    
+    OneSignal.init(isDev ? process.env.ONESIGNAL_APP_ID_DEBUG : process.env.ONESIGNAL_APP_ID_RELEASE, {
+      kOSSettingsKeyAutoPrompt : false,
+      kOSSettingsKeyInAppLaunchURL: false,
+      kOSSettingsKeyInFocusDisplayOption:2
+    })
+    OneSignal.inFocusDisplaying(2) // Controls what should happen if a notification is received while the app is open. 2 means that the notification will go directly to the device's notification center.
+    
+    // The promptForPushNotifications function code will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step below)
+    // OneSignal.promptForPushNotificationsWithUserResponse(myiOSPromptCallback)
+
+    OneSignal.addEventListener('received', this.onReceived)
+    OneSignal.addEventListener('opened', this.onOpened)
+    OneSignal.addEventListener('ids', this.onIds)
+  }
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived)
+    OneSignal.removeEventListener('opened', this.onOpened)
+    OneSignal.removeEventListener('ids', this.onIds)
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification)
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body)
+    console.log('Data: ', openResult.notification.payload.additionalData)
+    console.log('isActive: ', openResult.notification.isAppInFocus)
+    console.log('openResult: ', openResult)
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device)
+  }
+  
+  render () {
+      return <ErrorBoundary>
+      <Provider store={store}>
+        <RootView />
+      </Provider>
+    </ErrorBoundary>
+  }
+}
