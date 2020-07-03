5b6539e0e8d7f85e8ec3c1b04469cf553f756948
Loren Johnson <lorenjohnson@gmail.com>
2020-07-03 13:21:19 -1000

    Quick and dirty CircleCI config update

---
+++ ios/Pods/Headers/Private/react-native-intercom/IntercomEventEmitter.h
@@ -0,0 +1,1 @@
+../../../../../node_modules/react-native-intercom/iOS/IntercomEventEmitter.h
---
+++ ios/Pods/Target Support Files/Pods-HyloReactNativeTests/Pods-HyloReactNativeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HyloReactNativeTests : NSObject
+@end
+@implementation PodsDummy_Pods_HyloReactNativeTests
+@end
