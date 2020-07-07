c22d402eb243bec1b0467c6c9000dfe1e4d6ac74
Loren Johnson <lorenjohnson@gmail.com>
2020-07-07 02:36:40 -1000

    Update API_HOST options to accomdate QA builds

--- src/util/apiHost.js
+++ src/util/apiHost.js
@@ -3,13 +3,18 @@
  */
 
 import { isIOS } from 'util/platform'
-import { isDev } from '../config'
+import { isDev, isProduction } from '../config'
 
 const androidHost = isDev && !isIOS &&
   (process.env.ANDROID_API_HOST || process.env.ANDROID_EMULATOR_API_HOST)
 const iosHost = isDev && isIOS &&
   (process.env.IOS_API_HOST || process.env.IOS_EMULATOR_API_HOST)
-const HOST = androidHost || iosHost || process.env.API_HOST
+// Allows option of configuring .env with both an API_HOST,
+// which is used in QA and production builds, and a
+// DEV_API_HOST in which is used in the development environment
+// and builds
+const devHost = isDev && process.env.DEV_API_HOST
+const HOST = androidHost || iosHost || devHost || process.env.API_HOST
 
 export default HOST
 
