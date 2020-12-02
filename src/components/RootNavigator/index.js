720a51f9cf14d5d3184c7919dcc9942a716b8b7b
Loren Johnson <lorenjohnson@gmail.com>
2020-12-01 23:09:07 -1000

    Experimental nav config changes

--- src/components/RootNavigator/index.js
+++ src/components/RootNavigator/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import {
   createDrawerNavigator,
   createBottomTabNavigator,
@@ -5,6 +6,9 @@ import {
 } from 'react-navigation'
 import { Dimensions } from 'react-native'
 import { LoadingScreen } from '../Loading'
+import Header from 'components/Tabs/Header'
+import TabIcon from '../Tabs/TabIcon'
+import TabLabel from '../Tabs/TabLabel'
 import SessionCheck from '../SessionCheck'
 import Feed from '../Feed'
 import JoinCommunity from '../JoinCommunity'
@@ -43,7 +47,6 @@ import ForgotPassword from '../ForgotPassword'
 import CreateCommunityName from '../CreateCommunityFlow/CreateCommunityName'
 import CreateCommunityUrl from '../CreateCommunityFlow/CreateCommunityUrl'
 import CreateCommunityReview from '../CreateCommunityFlow/CreateCommunityReview'
-
 import InviteExpired from '../InviteExpired'
 import Signup from '../Signup'
 import trackCurrentTab from './trackCurrentTab'
@@ -51,105 +54,142 @@ import { isIOS } from 'util/platform'
 import { MAIN_ROUTE_NAME, MAIN_ROUTE_PATH } from 'util/navigation'
 import extendRouter from './extendRouter'
 
-// If you change or add tabs you have to edit trackCurrentTab.js
-const tabs = {
+const HomeStack = createStackNavigator({
+  // screensInTabs
+  // Screens that can be shown in any tab (the same tab icon stays highlighted)
   Home: {screen: Home, path: ''},
-  Members: {screen: Members, path: 'people'},
-  Topics: {screen: Topics, path: 'topics'},
-  Projects: {screen: Projects, path: 'projects'}
-}
-
-// Screens that can be shown in any tab (the same tab icon stays highlighted)
-const screensInTabs = {
   Feed: {screen: Feed, path: 'feed/:communityId'},
   TopicFeed: {screen: Feed, path: 'c/:communitySlugFromLink/topicFeed/:topicName'},
   CommunityFeed: {screen: Feed, path: 'communityFeed/:communitySlugFromLink'},
   NetworkFeed: {screen: Feed, path: 'networkFeed/:networkSlug'}
-}
+})
 
-Object.freeze(tabs)
-Object.freeze(screensInTabs)
-
-const TabNavigatorWithBar = createBottomTabNavigator(
-  stacksInTabsFactory(tabs, screensInTabs),
-  {
-    initialRouteName: 'Home',
-    tabBarPosition: 'bottom',
-    animationEnabled: false,
-    swipeEnabled: false,
-    lazy: true,
-    tabBarOptions: {
-      showIcon: true,
-      pressColor: '#DCDCDC',
-      showLabel: true,
-      indicatorStyle: {backgroundColor: 'white'},
-      style: isIOS ? tabStyles.tabNavigatorIOS : tabStyles.tabNavigatorAndroid
+const BottomTabs = createBottomTabNavigator({
+  Home: {
+    screen: HomeStack,
+    navigationOptions: {
+      tabBarIcon: ({ focused }) => (
+        <TabIcon name='Home' focused={focused} />
+      ),
+      tabBarLabel: ({ focused }) => (
+        <TabLabel name='Home' focused={focused} />
+      )
+    }
+  },
+  Members: {
+    screen: Members,
+    path: 'people',
+    navigationOptions: {
+      tabBarIcon: ({ focused }) => (
+        <TabIcon name='Members' focused={focused} />
+      ),
+      tabBarLabel: ({ focused }) => (
+        <TabLabel name='Members' focused={focused} />
+      )
+    }
+  },
+  Topics: {
+    screen: Topics,
+    path: 'topics',
+    navigationOptions: {
+      tabBarIcon: ({ focused }) => (
+        <TabIcon name='Topics' focused={focused} />
+      ),
+      tabBarLabel: ({ focused }) => (
+        <TabLabel name='Topics' focused={focused} />
+      )
+    }
+  },
+  Projects: {
+    screen: Projects,
+    path: 'projects',
+    navigationOptions: {
+      tabBarIcon: ({ focused }) => (
+        <TabIcon name='Projects' focused={focused} />
+      ),
+      tabBarLabel: ({ focused }) => (
+        <TabLabel name='Projects' focused={focused} />
+      )
     }
   }
-)
+},
+{
+  initialRouteName: 'Home',
+  tabBarPosition: 'bottom',
+  animationEnabled: false,
+  swipeEnabled: false,
+  lazy: true,
+  tabBarOptions: {
+    showIcon: true,
+    pressColor: '#DCDCDC',
+    showLabel: true,
+    indicatorStyle: {backgroundColor: 'white'},
+    style: isIOS ? tabStyles.tabNavigatorIOS : tabStyles.tabNavigatorAndroid
+  },
+  defaultNavigationOptions: ({ navigation }) => ({
+    title: 'teststelkjalskdjflsk'
+  })
+})
 
-const DrawerAndTabsNavigator = createDrawerNavigator(
+const DrawerAndBottomTabs = createDrawerNavigator(
   {
-    DrawerHome: {screen: TabNavigatorWithBar}
+    DrawerHome: BottomTabs
   },
   {
     contentComponent: DrawerMenu,
     initialRouteName: 'DrawerHome',
+    // hideStatusBar: true,
+    // drawerType: 'front',
     drawerWidth: Dimensions.get('window').width * 0.9
   }
 )
 
-// Screens that appear outside of tabs: Settings, Messages, etc.
-const screensInStack = {
-  MemberProfile: {screen: MemberProfile, path: 'people/:id'},
-  MemberDetails: {screen: MemberDetails},
-  MemberSkillEditor: {screen: MemberSkillEditor},
-  NewMessage: {screen: NewMessage},
-  PostDetails: {screen: PostDetails, path: 'post/:id'},
-  PostEditor: {screen: PostEditor},
-  ProjectMembers: {screen: ProjectMembers},
-  ItemChooserScreen: {screen: ItemChooserScreen},
-  UserSettings: {screen: UserSettings},
-  InvitePeople: {screen: InvitePeople},
-  ModeratorSettings: {screen: ModeratorSettings},
-  CommunitySettingsMenu: {screen: CommunitySettingsMenu},
-  CommunitySettings: {screen: CommunitySettings},
-  PasswordReset: {screen: UserSettings, path: 'settings/password'},
-  NotificationsList: {screen: NotificationsList},
-  ThreadList: {screen: ThreadList},
-  ThreadParticipants: {screen: ThreadParticipants},
-  TopicSupportComingSoon: {screen: TopicSupportComingSoon},
-  SessionCheck: {screen: SessionCheck},
-  InviteExpired: {screen: InviteExpired},
-  Signup: {screen: Signup},
-  SignupFlow1: {screen: SignupFlow1},
-  SignupFlow2: {screen: SignupFlow2},
-  SignupFlow3: {screen: SignupFlow3},
-  SignupFlow4: {screen: SignupFlow4},
-  SignupFlow5: {screen: SignupFlow5},
-  Login: {screen: Login, path: 'login'},
-  LoginByPasswordResetToken: {screen: Login, path: 'passwordResetTokenLogin/:userId/:loginToken/:nextURL'},
-  ForgotPassword: {screen: ForgotPassword, path: 'reset-password'},
-  Thread: {screen: Thread, path: 'thread/:id'},
-  UseInvitation: {screen: JoinCommunity, path: 'useInvitation/:token'},
-  UseAccessCode: {screen: JoinCommunity, path: 'useAccessCode/:slug/:accessCode'},
-  Loading: {screen: LoadingScreen},
-  CreateCommunityName: {screen: CreateCommunityName},
-  CreateCommunityUrl: {screen: CreateCommunityUrl},
-  CreateCommunityReview: {screen: CreateCommunityReview},
-  NotificationSettings: {screen: NotificationSettings},
-  BlockedUsers: {screen: BlockedUsers},
-  SearchPage: {screen: SearchPage}
-}
-
 const RootNavigator = createStackNavigator(
   {
     [MAIN_ROUTE_NAME]: {
-      screen: DrawerAndTabsNavigator,
+      screen: DrawerAndBottomTabs,
       path: MAIN_ROUTE_PATH,
-      navigationOptions: {header: null}
     },
-    ...screensInStack
+    // Screens that appear outside of tabs: Settings, Messages, etc.
+    MemberProfile: {screen: MemberProfile, path: 'people/:id'},
+    MemberDetails,
+    MemberSkillEditor,
+    NewMessage,
+    PostDetails: {screen: PostDetails, path: 'post/:id'},
+    PostEditor,
+    ProjectMembers,
+    ItemChooserScreen,
+    UserSettings,
+    InvitePeople,
+    ModeratorSettings,
+    CommunitySettingsMenu,
+    CommunitySettings,
+    PasswordReset: {screen: UserSettings, path: 'settings/password'},
+    NotificationsList,
+    ThreadList,
+    ThreadParticipants,
+    TopicSupportComingSoon,
+    SessionCheck,
+    InviteExpired,
+    Signup,
+    SignupFlow1,
+    SignupFlow2,
+    SignupFlow3,
+    SignupFlow4,
+    SignupFlow5,
+    Login: {screen: Login, path: 'login'},
+    LoginByPasswordResetToken: {screen: Login, path: 'passwordResetTokenLogin/:userId/:loginToken/:nextURL'},
+    ForgotPassword: {screen: ForgotPassword, path: 'reset-password'},
+    Thread: {screen: Thread, path: 'thread/:id'},
+    UseInvitation: {screen: JoinCommunity, path: 'useInvitation/:token'},
+    UseAccessCode: {screen: JoinCommunity, path: 'useAccessCode/:slug/:accessCode'},
+    Loading: LoadingScreen,
+    CreateCommunityName,
+    CreateCommunityUrl,
+    CreateCommunityReview,
+    NotificationSettings,
+    BlockedUsers,
+    SearchPage
   },
   {
     cardStyle: {backgroundColor: '#FFF'},
