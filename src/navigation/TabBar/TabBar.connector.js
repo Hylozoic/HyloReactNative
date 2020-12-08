6fa43e6d68708e6dd1c9a91f5e6639c41813374c
Loren Johnson <lorenjohnson@gmail.com>
2020-12-08 05:18:17 -1000

    Fix Search, remove unused legacy component

--- src/navigation/TabBar/TabBar.connector.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux'
-
-function mapStateToProps (state) {
-  return {
-    isVisible: state.TabBar.isVisible
-  }
-}
-
-export default connect(mapStateToProps)
--- src/navigation/TabBar/index.js
+++
@@ -1,3 +0,0 @@
-import connector from './TabBar.connector'
-import component from './TabBar'
-export default connector(component)
