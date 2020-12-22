32488abba92cca80a58bdab2cb7d5d74d445414c
Loren Johnson <lorenjohnson@gmail.com>
2020-12-22 00:05:19 -1000

    Fix no loading indicators in Android

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { View, ActivityIndicator } from 'react-native'
+import { gunsmoke } from 'style/colors'
 
 export default function Loading ({ style }) {
   return (
     <View style={[style, styles.container]}>
-      <ActivityIndicator />
+      {/*
+        NOTE: 'color' prop is required due to RN bug in 0.63.4.
+        Without it being explcitely set the indicator will be invisble in Android.
+        Remove once the bug is fixed: https://github.com/facebook/react-native/pull/29830
+      */}
+      <ActivityIndicator color={gunsmoke} />
     </View>
   )
 }
