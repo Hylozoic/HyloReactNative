f272b58a769564a6d4d19a4c9cf80cbdbe4bca46
Loren Johnson <lorenjohnson@gmail.com>
2020-12-22 10:24:43 -1000

    Header normalization

--- src/navigation/header/index.js
+++ src/navigation/header/index.js
@@ -3,14 +3,16 @@ import { View } from 'react-native'
 import { HeaderBackButton } from '@react-navigation/stack'
 import { getFocusedRouteNameFromRoute } from '@react-navigation/native'
 import confirmDiscardChanges from 'util/confirmDiscardChanges'
+import { isIOS } from 'util/platform'
 import HeaderRightButton from 'navigation/header/HeaderRightButton'
 import HeaderLeftCloseIcon from 'navigation/header/HeaderLeftCloseIcon'
 import MenuButton from 'navigation/header/MenuButton'
 import SearchIcon from 'navigation/header/SearchIcon'
 import MessagesIcon from 'navigation/header/MessagesIcon'
 import NotificationsIcon from 'navigation/header/NotificationsIcon'
-import { black10onRhino, caribbeanGreen, white60onCaribbeanGreen } from 'style/colors'
-import styles from './header.styles'
+import { 
+  black10onRhino, caribbeanGreen, white60onCaribbeanGreen, white, rhino80
+} from 'style/colors'
 
 // Keep in sync with stack navigator root name changes in navigation/TabsNavigator
 // This won't be necessary once fully custom header is built
@@ -35,14 +37,32 @@ export function buildTabStackScreenOptions ({
     headerBackTitleVisible: false,
     headerTitle: getFocusedRouteNameFromRoute(route)
       || route?.name,
-    headerTitleContainerStyle: styles.headerTitleContainerStyle,
-    headerTitleStyle: styles.headerTitleStyle,
+    headerTitleContainerStyle: {
+      // // Follow: https://github.com/react-navigation/react-navigation/issues/7057#issuecomment-593086348
+      // width: isIOS ? '40%' : '75%',
+      // alignItems: isIOS ? 'center' : 'flex-start'
+      marginLeft: isIOS ? 0 : 10
+    },
+    headerTitleStyle: {
+      color: white,
+      fontFamily: 'Circular-Bold',
+      fontSize: 18,
+      fontWeight: '200'
+    },
     headerTitleAlign: 'center',
-    headerStyle: styles.headerStyle,
+    headerStyle: {
+      backgroundColor: rhino80
+      // backgroundColor: black10onRhino
+      // backgroundColor: black10OnCaribbeanGreen
+    },  
     headerLeft: () =>
       <MenuButton atRoot={atRoot} navigation={navigation} />,
     headerRight: () => atRoot && (
-      <View style={styles.headerRight}>
+      <View style={{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+      }}>
         <SearchIcon showSearch={() => navigation.navigate('Search')} />
         <MessagesIcon showMessages={() => navigation.navigate('Messages')} />
         <NotificationsIcon showNotifications={() => navigation.navigate('Notifications')} />
