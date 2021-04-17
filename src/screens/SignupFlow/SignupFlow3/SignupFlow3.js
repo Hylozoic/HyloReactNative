232645f25f3c2e3c4b0ca8340e40cdafc3b0578e
Loren Johnson <lorenjohnson@gmail.com>
2021-04-16 23:57:23 -1000

    Add Location picker to User Location signup step

--- src/screens/SignupFlow/SignupFlow3/SignupFlow3.js
+++ src/screens/SignupFlow/SignupFlow3/SignupFlow3.js
@@ -4,7 +4,9 @@ import SafeAreaView from 'react-native-safe-area-view'
 import Button from 'components/Button'
 import KeyboardFriendlyView from 'components/KeyboardFriendlyView'
 import SettingControl from 'components/SettingControl'
+import LocationPicker from 'screens/LocationPicker/LocationPicker'
 import styles from './SignupFlow3.styles'
+
 
 export default class SignupFlow3 extends React.Component {
   controlRef = React.createRef()
@@ -14,8 +16,21 @@ export default class SignupFlow3 extends React.Component {
     this.props.saveAndNext()
   }
 
+  showLocationPicker = locationText  => {
+    const { navigation, changeSetting } = this.props
+
+    LocationPicker({
+      navigation,
+      initialSearchTerm: locationText,
+      onPick: location => {
+        changeSetting('location')(location?.fullText)
+        changeSetting('locationId')(location?.id)
+      }
+    })
+  }
+
   render () {
-    const { location, changeSetting } = this.props
+    const { location } = this.props
 
     return (
       <SafeAreaView style={styles.container}>
@@ -32,7 +47,7 @@ export default class SignupFlow3 extends React.Component {
                 ref={this.controlRef}
                 label='Where do you call home'
                 value={location}
-                onChange={changeSetting('location')}
+                onFocus={() => this.showLocationPicker(location)}
               />
             </View>
             <View style={styles.footer}>
