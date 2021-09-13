80239bd5cc27df0c14887a46c440eae82e62244d
Loren Johnson <lorenjohnson@gmail.com>
2021-09-13 14:11:50 -0700

    Fixes bug with not being able to exit signup to login

--- src/screens/Login/Login.connector.js
+++ src/screens/Login/Login.connector.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux'
 import {
+  logout,
   login,
   loginWithApple,
   loginWithFacebook,
@@ -8,6 +9,8 @@ import {
 } from './actions'
 import { getPending } from './Login.store'
 import getRouteParam from 'store/selectors/getRouteParam'
+import getSignupInProgress from 'store/selectors/getSignupInProgress'
+import { buildLoadCurrentUserSession } from 'navigation/RootView/RootView.connector'
 
 export function mapStateToProps (state, props) {
   const formError = state.session.loginError
@@ -16,33 +19,33 @@ export function mapStateToProps (state, props) {
   const goToResetPassword = () => props.navigation.navigate('ForgotPassword')
   return {
     pending,
-    signupInProgress: getRouteParam('signupInProgress', props.route),
+    signupInProgress: getSignupInProgress(state),
     bannerMessage: getRouteParam('bannerMessage', props.route),
     formError,
-    defaultEmail: state.session.defaultLoginEmail,
+    defaultEmail: state.session?.defaultLoginEmail,
     goToSignup,
     goToResetPassword,
     loginToken: decodeURIComponent(getRouteParam('loginToken', props.route)),
-    loginTokenUserId: getRouteParam('userId', props.route),
-    // TODO: May bring back DeepLink
-    // deepLinkAction: getNavigationAction(state)
+    loginTokenUserId: getRouteParam('userId', props.route)
   }
 }
 
 export const mapDispatchToProps = {
+  logout,
+  login,
   loginWithApple,
   loginWithFacebook,
   loginWithGoogle,
-  login,
   loginByToken
 }
 
 export function mergeProps (stateProps, dispatchProps, ownProps) {
   const {
+    logout,
+    login,
     loginWithApple,
     loginWithGoogle,
     loginWithFacebook,
-    login,
     loginByToken
   } = dispatchProps
   const finishLogin = async (action) => {
@@ -56,19 +59,34 @@ export function mergeProps (stateProps, dispatchProps, ownProps) {
     ...ownProps,
     ...stateProps,
     ...dispatchProps,
-    loginWithApple: (token) =>
-      loginWithApple(token).then(finishLogin),
-    loginWithFacebook: (token) =>
-      loginWithFacebook(token).then(finishLogin),
-    loginWithGoogle: (token) =>
-      loginWithGoogle(token).then(finishLogin),
-    loginByToken: () => {
-      const { loginTokenUserId, loginToken } = stateProps
-      return loginTokenUserId && loginByToken(loginTokenUserId, loginToken)
-        .then(finishLogin)
+    loginWithApple: async token => {
+      await logout()
+      const action = await loginWithApple(token)
+      return finishLogin(action)
     },
-    login: (email, password) =>
-      login(email, password).then(finishLogin)
+    loginWithFacebook: async token => {
+      await logout()
+      const action = await loginWithFacebook(token)
+      return finishLogin(action)
+    },
+    loginWithGoogle: async token => {
+      await logout()
+      const action = await loginWithGoogle(token)
+      return finishLogin(action)
+    },
+    loginByToken: async () => {
+      const { loginTokenUserId, loginToken } = stateProps
+      if (loginTokenUserId) {
+        await logout()
+        const action = await loginByToken(loginTokenUserId, loginToken)
+        return finishLogin(action)
+      }
+    },
+    login: async (email, password) => {
+      await logout()
+      const action = await login(email, password)
+      return finishLogin(action)
+    }
   }
 }
 
