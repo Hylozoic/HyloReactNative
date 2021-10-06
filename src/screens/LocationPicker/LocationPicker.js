b4d83ac2feff4efaa6a24af0dd5952ccadc186a4
Loren Johnson <lorenjohnson@gmail.com>
2021-10-06 02:10:31 -0700

    Remove unnecessary comments

--- src/screens/LocationPicker/LocationPicker.js
+++ src/screens/LocationPicker/LocationPicker.js
@@ -1,11 +1,10 @@
-// Location Picker
 import { locationSearch } from 'screens/LocationPicker/LocationPicker.store'
 import LocationPickerItemRow from 'screens/LocationPicker/LocationPickerItemRow'
 
 export default function LocationPicker ({
   screenTitle = 'Choose a Location',
   navigation,
-  // TODO: Defaut to current location to send as proximity for location search
+  // TODO: Default to current location to send as proximity for location search
   initialSearchTerm = '',
   onPick
 }) {
