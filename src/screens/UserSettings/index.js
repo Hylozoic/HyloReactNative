b9313e3d18cf76891deeb534d981e4eaa0291064
Loren Johnson <lorenjohnson@gmail.com>
2021-11-01 13:05:20 -0700

    Reset curently unused User Settings component

--- src/screens/UserSettings/index.js
+++ src/screens/UserSettings/index.js
@@ -1,4 +1,4 @@
 import component from './UserSettings'
-// import connector from './UserSettings.connector'
+import connector from './UserSettings.connector'
 
-export default component
+export default connector(component)
