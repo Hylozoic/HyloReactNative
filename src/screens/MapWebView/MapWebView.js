aea57fe180f7fe80d5af14fb000f3bca9c9c4edf
Loren Johnson <lorenjohnson@gmail.com>
2022-01-17 17:01:59 -0800

    Code comments

--- src/screens/MapWebView/MapWebView.js
+++ src/screens/MapWebView/MapWebView.js
@@ -7,10 +7,11 @@ import useGroupSelect from 'hooks/useGroupSelect'
 import getCurrentGroup from 'store/selectors/getCurrentGroup'
 import HyloWebView from 'screens/HyloWebView'
 
-// TODO: Move these into common library (probably navigation/linking/helpers.js)
-// MATCHER_GROUP_SLUG matches only actual group paths (e.g. not /all or /public)
+// Matches actual group paths (e.g. not /all or /public)
 export const MATCHER_GROUP_SLUG = '[a-zA-Z0-9\-]+$'
 export const MATCHER_GROUP_ROOT_PATH = `\/groups\/${MATCHER_GROUP_SLUG}$`
+
+// Matches special group paths (e.g. /all and /public)
 export const MATCHER_GROUP_ALL_AND_PUBLIC_ROOT_PATH = `\/(${ALL_GROUP_ID}|${PUBLIC_GROUP_ID})$`
 
 export default function MapWebView ({ navigation }) {
@@ -18,7 +19,8 @@ export default function MapWebView ({ navigation }) {
   const group = useSelector(getCurrentGroup)
   const [path, setPath] = useState()
 
-  // Going to the map will switch currentGroup context
+  // Navigating to map switchs currentGroup context to the currently
+  // selected group or the one found in the groupId route param
   useGroupSelect()
 
   useFocusEffect(
@@ -28,7 +30,7 @@ export default function MapWebView ({ navigation }) {
         // Disables going back by pull right on this screen
         gestureEnabled: false
       })
-      // DisablesswipeEnabled on DrawerNavigator
+      // Disables swipeEnabled on DrawerNavigator
       navigation.getParent()?.getParent()?.setOptions({ swipeEnabled: false })
       if ([ALL_GROUP_ID, PUBLIC_GROUP_ID].includes(group?.slug)) {
         setPath(() => `${group?.slug}/map`)
@@ -42,22 +44,40 @@ export default function MapWebView ({ navigation }) {
 
   const onMessage = event => {
     const  { url } = JSON.parse(event.nativeEvent.data)
-    // Special case to re-write route for Member details in a group context:
-    //   /groups/my-lovely-group/members/<member-id>
-    // ...to go to context-free Member Detail modal:
-    //   /all/member/<member-id>
-    if (url?.match(/\/groups\/*.+\/members\/*.+$/)) {
+    
+    // DISABLED FOR NOW, SEE: https://github.com/Hylozoic/HyloReactNative/issues/521
+    // Matches: '*/map' from Web react-router push events
+    // ignores as they are currently only savedSearch retrievals.
+    // navigating to the linking path here in the App will reload the map
+    // and switch to the targetted group context without the saved search
+    // parameters.
+    //
+    // * For now this leaves the user in a state of no longer
+    // being in the group which the screen header shows
+    // if (url.match(/\/map$/)) {
+    //   return
+    // }
+
+    // Matches: groups/my-awesome-group/members/<member-id> or /all|pubic/members/<member-id>
+    // re-writes linking to go to Member details modal in the all context
+    if (url.match(/\/groups\/*.+\/members\/*.+$/)) {
       const memberModalPath = '/all/' + url.split('/').slice(3,5).join('/')
       navigateToLinkingPathInApp(memberModalPath)
+    
     // Matches: /groups/our-awesome-group/map/post/<post-id>, /(all|public)/post/<post-id>
-    } else if (url?.match(/\/post|\/members/)) {
+    } else if (url.match(/\/post|\/members/)) {
       navigateToLinkingPathInApp(url)
-    // Matches: /groups/our-awesome-group, /all, /public
+    
+    // Matches: /groups/our-awesome-group
     // re-writes linking to go to Group Detail modal
     } else if (url.match(new RegExp(MATCHER_GROUP_ROOT_PATH))) {
       navigateToLinkingPathInApp(url + '/detail')
+
+    // Matches: /all, /public
+    // re-writes linking to stays on map reloading it in the target context
     } else if (url.match(new RegExp(MATCHER_GROUP_ALL_AND_PUBLIC_ROOT_PATH))) {
       navigateToLinkingPathInApp(url + '/map')
+    
     } else {
       // NOTE: Right now this captures saved search view calls, may capture too much?
       navigateToLinkingPathInApp(url)
