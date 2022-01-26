d418202ce341bc12c148d60978e58aac390a2503
Loren Johnson <lorenjohnson@gmail.com>
2022-01-25 16:05:08 -0800

    Add GraphQL tag support and config for VS Code GraphQL related Plugins

--- src/store/actions/fetchCurrentUser.js
+++ src/store/actions/fetchCurrentUser.js
@@ -1,160 +1,163 @@
 import { FETCH_CURRENT_USER } from 'store/constants'
+import gql from 'graphql-tag'
 
 export default function fetchCurrentUser () {
   return {
     type: FETCH_CURRENT_USER,
     graphql: {
-      query: `{
-        me {
-          id
-          name
-          email
-          bio
-          tagline    
-          location
-          locationObject {
-            id
-            addressNumber
-            addressStreet
-            bbox {
-              lat
-              lng
-            }
-            center {
-              lat
-              lng
-            }
-            city
-            country
-            fullText
-            locality
-            neighborhood
-            region
-          }
-          avatarUrl
-          facebookUrl
-          twitterName
-          newNotificationCount
-          unseenThreadCount
-          intercomHash
-          skills (first: 100) {
-            total
-            hasMore
-            items {
-              id
-              name
-            }
-          }
-          blockedUsers {
+      query: gql`
+        query MeQuery {
+          me {
             id
             name
-          }
-          settings {
-            signupInProgress
-            dmNotifications
-          }
-          joinRequests(status: 0) {
-            items {
+            email
+            bio
+            tagline    
+            location
+            locationObject {
               id
-              status
-              createdAt
-              group {
+              addressNumber
+              addressStreet
+              bbox {
+                lat
+                lng
+              }
+              center {
+                lat
+                lng
+              }
+              city
+              country
+              fullText
+              locality
+              neighborhood
+              region
+            }
+            avatarUrl
+            facebookUrl
+            twitterName
+            newNotificationCount
+            unseenThreadCount
+            intercomHash
+            skills (first: 100) {
+              total
+              hasMore
+              items {
+                id
+                name
+              }
+            }
+            blockedUsers {
+              id
+              name
+            }
+            settings {
+              signupInProgress
+              dmNotifications
+            }
+            joinRequests(status: 0) {
+              items {
+                id
+                status
+                createdAt
+                group {
+                  id
+                }
+              }
+            }
+            affiliations {
+              items {
+                id
+                role
+                preposition
+                orgName
+                url
+                createdAt
+                updatedAt
+                isActive
+              }
+            }
+            memberships {
+              id
+              lastViewedAt
+              newPostCount
+              hasModeratorRole
+              settings {
+                sendEmail
+                sendPushNotifications
+              }
+              person {
                 id
               }
-            }
-          }
-          affiliations {
-            items {
-              id
-              role
-              preposition
-              orgName
-              url
-              createdAt
-              updatedAt
-              isActive
-            }
-          }
-          memberships {
-            id
-            lastViewedAt
-            newPostCount
-            hasModeratorRole
-            settings {
-              sendEmail
-              sendPushNotifications
-            }
-            person {
-              id
-            }
-            group {
-              accessibility
-              avatarUrl
-              bannerUrl
-              description
-              id
-              memberCount
-              name
-              slug
-              visibility
-              childGroups(first: 300) {
-                items {
-                  accessibility
-                  avatarUrl
-                  bannerUrl
-                  description
-                  id
-                  memberCount
-                  name
-                  slug
-                  visibility
-                  settings {
-                    allowGroupInvites
-                    askJoinQuestions
-                    publicMemberDirectory
-                  }
-                }
-              }
-              groupTopics(subscribed: true) {
-                total
-                hasMore
-                items {
-                  followersTotal
-                  id
-                  isSubscribed
-                  newPostCount
-                  postsTotal
-                  topic {
+              group {
+                accessibility
+                avatarUrl
+                bannerUrl
+                description
+                id
+                memberCount
+                name
+                slug
+                visibility
+                childGroups(first: 300) {
+                  items {
+                    accessibility
+                    avatarUrl
+                    bannerUrl
+                    description
                     id
+                    memberCount
                     name
+                    slug
+                    visibility
+                    settings {
+                      allowGroupInvites
+                      askJoinQuestions
+                      publicMemberDirectory
+                    }
                   }
                 }
-              }
-              parentGroups {
-                items {
-                  accessibility
-                  avatarUrl
-                  bannerUrl
-                  description
-                  id
-                  memberCount
-                  name
-                  slug
-                  visibility
-                  settings {
-                    allowGroupInvites
-                    askJoinQuestions
-                    publicMemberDirectory
+                groupTopics(subscribed: true) {
+                  total
+                  hasMore
+                  items {
+                    followersTotal
+                    id
+                    isSubscribed
+                    newPostCount
+                    postsTotal
+                    topic {
+                      id
+                      name
+                    }
                   }
                 }
+                parentGroups {
+                  items {
+                    accessibility
+                    avatarUrl
+                    bannerUrl
+                    description
+                    id
+                    memberCount
+                    name
+                    slug
+                    visibility
+                    settings {
+                      allowGroupInvites
+                      askJoinQuestions
+                      publicMemberDirectory
+                    }
+                  }
+                }
+                settings {
+                  allowGroupInvites
+                }
               }
-              settings {
-                allowGroupInvites
-              }
-            }
-          }          
+            }          
+          }
         }
-      }`
+      `
     },
     meta: {
       afterInteractions: true,
