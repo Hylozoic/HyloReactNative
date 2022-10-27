1e4361af750421e33208d1d9f1ac151bba32d0da
Loren Johnson <lorenjohnson@gmail.com>
2022-10-27 01:24:13 -0700

    Removes native Android Popup option as the `useRef` here was breaking many tests and we don't use the native Android Popup anymore

--- src/components/PopupMenuButton/PopupMenuButton.js
+++ src/components/PopupMenuButton/PopupMenuButton.js
@@ -5,43 +5,13 @@ import useHyloActionSheet from 'hooks/useHyloActionSheet'
 
 export default function PopupMenuButton ({
   actions,
-  // If you prefer the PopupMenu Style for Android
-  useAndroidPopup,
   children,
   viewProps,
   style,
   hitSlop = { top: 5, bottom: 5, left: 5, right: 5 }
 }) {
   const { showHyloActionSheet } = useHyloActionSheet()
-  const buttonRef = useRef()
-  const [open, setOpen] = useState()
-
-  const showAndroidPopup = () => {
-    if (buttonRef.current && !open) {
-      UIManager.showPopupMenu(
-        findNodeHandle(buttonRef.current),
-        actions.map(a => a[0]),
-        // onError
-        e => {
-          setOpen(false)
-
-          throw new Error('Error opening popup')
-        },
-        // onSelect
-        (action, index) => {
-          setOpen(false)
-
-          if (action === 'itemSelected') {
-            actions[index][1]()
-          }
-        }
-      )
-    }
-  }
-
-  const handlePress = !isIOS && useAndroidPopup
-    ? showAndroidPopup
-    : () => showHyloActionSheet({ actions })
+  const handlePress = () => showHyloActionSheet({ actions })
 
   return (
     <TouchableOpacity
@@ -49,7 +19,6 @@ export default function PopupMenuButton ({
       style={style}
       hitSlop={hitSlop}
       {...viewProps}
-      ref={buttonRef}
     >
       {children}
     </TouchableOpacity>
