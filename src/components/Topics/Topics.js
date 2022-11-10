e9ae4649c2eb9222e70df49cded68d21a314e93f
Loren Johnson <lorenjohnson@gmail.com>
2022-11-10 12:35:23 -0800

    Add PillStyle to Topics for PostCard use

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -2,13 +2,20 @@ import { ScrollView, Text, TouchableOpacity } from 'react-native'
 import Icon from 'components/Icon'
 import { amaranth, caribbeanGreen, rhino30 } from 'style/colors'
 
-export default function Topics ({ onPress, onPressRemove, topics, style }) {
+export default function Topics ({
+  onPress,
+  onPressRemove,
+  topics,
+  style,
+  pillStyle
+}) {
   if (topics.length < 1) return null
 
   return (
     <ScrollView horizontal style={[styles.topicPillBox, style]}>
       {topics.map((topic, index) => (
         <TopicPill
+          style={pillStyle}
           topic={topic}
           onPress={onPress}
           onPressRemove={onPressRemove}
@@ -19,7 +26,12 @@ export default function Topics ({ onPress, onPressRemove, topics, style }) {
   )
 }
 
-export function TopicPill ({ topic, topic: { name }, onPress, onPressRemove }) {
+export function TopicPill ({
+  topic, topic: { name },
+  style,
+  onPress,
+  onPressRemove
+}) {
   const handleOnPress = onPress
     ? () => onPress(topic)
     : () => {}
@@ -28,7 +40,7 @@ export function TopicPill ({ topic, topic: { name }, onPress, onPressRemove }) {
     : () => {}
 
   return (
-    <TouchableOpacity onPress={handleOnPress} style={styles.topicPill}>
+    <TouchableOpacity onPress={handleOnPress} style={[styles.topicPill, style]}>
       <Text style={styles.topicText}>#{name}</Text>
       <TouchableOpacity onPress={handleOnPressRemove}>
         <Icon name='Ex' style={styles.topicRemove} />
