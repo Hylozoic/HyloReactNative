0a69c479695029dd3d8c8fe95e8470d89e32f853
Loren Johnson <lorenjohnson@gmail.com>
2024-06-24 23:41:57 +0200

    Fixes "shadow" React Native warning (#749)

--- src/components/FeedList/PostRow/PostRow.styles.js
+++ src/components/FeedList/PostRow/PostRow.styles.js
@@ -6,18 +6,6 @@ export default StyleSheet.create({
   postRow: {
     paddingBottom: 12,
     marginRight: feedMargin,
-    marginLeft: feedMargin,
-
-    // Card Box Shadow from Web:
-    // 0 4px 10px rgba(35, 65, 91, 0.3)
-    // https://ethercreative.github.io/react-native-shadow-generator
-    shadowColor: 'rgba(35, 65, 91, 0.3)',
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.22,
-    shadowRadius: 2.22,
-    elevation: 3
+    marginLeft: feedMargin
   }
 })
