3d9bb50ab9e73e2fc0234f51729609acbda59d41
Tom <thomasgabriel.watson@gmail.com>
2024-09-08 15:20:57 -0700

    Review tweak

--- src/store/selectors/getResponsibilitiesForGroup.js
+++ src/store/selectors/getResponsibilitiesForGroup.js
@@ -1,13 +1,14 @@
 import orm from '../models'
 import { createSelector as ormCreateSelector } from 'redux-orm'
 import getMe from './getMe'
+import { isContextGroup } from 'store/models/Group'
 
 const getResponsibilitiesForGroup = ormCreateSelector(
   orm,
   (state, props) => props.person || getMe(state),
   (state, props) => props.groupId,
   ({ CommonRole }, person, groupId) => {
-    if (!person || !groupId || ['all', 'public'].includes(groupId)) return []
+    if (!person || !groupId || isContextGroup(groupId)) return []
     const commonRoles = CommonRole.all().toModelArray()
     const membershipCommonRoles = person.membershipCommonRoles.items.filter(mcr => mcr.groupId === groupId)
     const commonResp = commonRoles.filter(cr => membershipCommonRoles.find(mcr => mcr.commonRoleId === cr.id)).map(cr => cr.responsibilities.items).flat()
